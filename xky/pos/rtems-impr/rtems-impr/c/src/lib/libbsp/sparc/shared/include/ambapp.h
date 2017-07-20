f8564d7137930fb88c53f5210bed2b1d3a03c819
Luis Murta <assert@tastevm.estec.esa.int>
2017-07-20 16:09:13 +0200

    spw rev6. it compiles!

--- xky/pos/rtems-impr/rtems-impr/c/src/lib/libbsp/sparc/shared/include/ambapp.h
+++ xky/pos/rtems-impr/rtems-impr/c/src/lib/libbsp/sparc/shared/include/ambapp.h
@@ -102,7 +102,7 @@ extern "C"
 #define GAISLER_FTMCTRL   0x54
 #define GAISLER_BRM       0x72
 #define GAISLER_GRSPW2_DMA 0x8a
-
+#define GAISLER_GRSPW_ROUTER 0x08b
 
    /* European Space Agency device id's */
 #define ESA_LEON2        0x2
