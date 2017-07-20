d93d3fefe58b8da073d3be1a08c27e255597bba5
Luís Murta Mendes <trlumm@gmv.es>
2017-07-20 16:16:13 +0100

    spw rev7

--- xky/tools/configurator/parsers/iop/devices.py
+++ xky/tools/configurator/parsers/iop/devices.py
@@ -11,6 +11,7 @@ iop_supported_devices = {
     # device        #type   # physical dev setup    # scheduler dev setup   # template file
     'GRETH'     : [ ETH,    gaisler.phy_greth,      gaisler.sch_greth,      'iop_greth.mako' ],
     'CPSW'      : [ ETH,    ti.phy_cpsw,            ti.sch_cpsw,            'iop_cpsw.mako'  ],
-    'GRSPW'     : [ ETH,    gaisler.phy_grspw,      gaisler.sch_grspw,      'iop_grspw'],
+    'GRSPW'     : [ SPW,    gaisler.phy_grspw,      gaisler.sch_grspw,      'iop_grspw.mako'],
+#    'SPWRTR'    : [ SPW,    gaisler.phy_spwrtr,     gaisler.sch_spwrtr,     'iop_spw_router.mako'],
 
 }
