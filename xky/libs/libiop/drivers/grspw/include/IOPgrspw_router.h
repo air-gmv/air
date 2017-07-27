da17d01152f0e3e1539cf4b660e925f1a4a4f188
Luís Murta Mendes <trlumm@gmv.es>
2017-07-27 13:51:40 +0100

    spwrtr rev12

--- xky/libs/libiop/drivers/grspw/include/IOPgrspw_router.h
+++ xky/libs/libiop/drivers/grspw/include/IOPgrspw_router.h
@@ -35,7 +35,7 @@ struct router_hw_info {
 #define ROUTER_FLG_TRLD		0x10
 #define ROUTER_FLG_ALL		0x1f	/* All Above Flags */
 
-struct router_config {
+typedef struct {
 	unsigned int flags; /* Determine what configuration should be updated */
 
 	/* Router Configuration Register */
@@ -50,61 +50,29 @@ struct router_config {
 	/* Timer Prescaler and Reload */
 	unsigned int timer_prescaler;
 	unsigned int timer_reload[32];
-};
+} router_config_t;
 
 /* Logical routing table */
-struct router_routes {
+typedef struct {
 	unsigned int route[224];
-};
+} router_routes;
 
 /* Port Setup, see register definitions for "Port setup register" */
-struct router_ps {
+typedef struct {
 	unsigned int ps[31]; /* Port Setup for ports 1-31 */
 	unsigned int ps_logical[224]; /* Port setup for locgical addresses 32-255 */
-};
+} router_ps;
 
 /* Physical ports timeout */
-struct port_timer {
+typedef struct{
 	unsigned int timeout[32];
-};
+} port_timer;
 
 /* Set/Get Port Control/Status */
 #define ROUTER_PORTFLG_SET_CTRL	0x01
 #define ROUTER_PORTFLG_GET_CTRL	0x02
 #define ROUTER_PORTFLG_SET_STS	0x04
 #define ROUTER_PORTFLG_GET_STS	0x08
-struct router_port {
-	unsigned int flag;
-	int port;
-	unsigned int ctrl;
-	unsigned int sts;
-};
-
-/* SpaceWire registry fields */
-struct router_regs {
-	unsigned int resv1;		/* 0x000 */
-	unsigned int psetup[255];	/* 0x004 */
-	unsigned int resv2[32];		/* 0x400 */
-	unsigned int routes[224];	/* 0x480 */
-	unsigned int pctrl[32];		/* 0x800 */
-	unsigned int psts[32];		/* 0x880 */
-	unsigned int treload[32];	/* 0x900 */
-	unsigned int resv3[32];		/* 0x980 */
-	unsigned int cfgsts;		/* 0xA00 */
-	unsigned int timecode;		/* 0xA04 */
-	unsigned int ver;		/* 0xA08 */
-	unsigned int idiv;		/* 0xA0C */
-	unsigned int cfgwe;		/* 0xA10 */
-	unsigned int tprescaler;	/* 0xA14 */
-	unsigned int resv4[123];	/* 0xA18 */
-	unsigned int charo[31];		/* 0xC04 */
-	unsigned int resv5;		/* 0xC80 */
-	unsigned int chari[31];		/* 0xC84 */
-	unsigned int resv6;		/* 0xD00 */
-	unsigned int pkto[31];		/* 0xD04 */
-	unsigned int resv7;		/* 0xD80 */
-	unsigned int pkti[31];		/* 0xD84 */
-};
 
 typedef struct {
 	char devName[32];
@@ -122,8 +90,8 @@ typedef struct {
 #define GRSPWR_IOCTL_HWINFO	0x01	/* OUT:    struct router_hw_info */
 
 /* Router Configuration */
-#define GRSPWR_IOCTL_CFG_SET	0x02	/* IN:     struct router_config */
-#define GRSPWR_IOCTL_CFG_GET	0x03	/* OUT:    struct router_config */
+#define GRSPWR_IOCTL_CFG_SET	0x02	/* IN:     router_config_t */
+#define GRSPWR_IOCTL_CFG_GET	0x03	/* OUT:    router_config_t */
 
 /* Routes */
 #define GRSPWR_IOCTL_ROUTES_SET	0x04	/* IN:     struct router_routes */
@@ -157,9 +125,9 @@ static rtems_device_driver router_control(iop_device_driver_t *iop_dev, void *ar
 
 static void router_hwinfo(router_priv_t *priv,	struct router_hw_info *hwinfo);
 
-static int router_config_set(router_priv_t *priv, struct router_config *cfg);
+static int router_config_set(router_priv_t *priv, router_config_t *cfg);
 
-static int router_config_read(router_priv_t *priv, struct router_config *cfg);
+static int router_config_read(router_priv_t *priv, router_config_t *cfg);
 
 static int router_routes_set(router_priv_t *priv, struct router_routes *routes);
 
