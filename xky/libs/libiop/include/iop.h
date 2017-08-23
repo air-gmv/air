ff8d35dabb4706f69e66796513ef5545c0901977
Luís Murta Mendes <trlumm@gmv.es>
2017-08-23 15:33:44 +0100

    Last commit for the spacewire drivers/grspw/IOPgrspw
    Spacewire packet must contain an header byte corresponding to the AMBA port
    receiving it. Offsets altered to include this consideration.
    Debugging cleanup and defines are on iop.h

--- xky/libs/libiop/include/iop.h
+++ xky/libs/libiop/include/iop.h
@@ -272,7 +272,8 @@ extern iop_configuration_t usr_configuration;
 
 void iop_main_loop(void);
 
-
+//#define DEBUG_SPWRTR
+//#define DEBUG_SPACEWIRE_ONOFF
 #define IOP_NEEDS_DEBUG
 #ifdef IOP_NEEDS_DEBUG
 #include <pprintf.h>
