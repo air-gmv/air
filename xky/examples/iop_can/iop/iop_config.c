3a721d2bee78562cfbd6c7f354d3d15e949d121a
Guilherme Sanches <guilherme.sanches@gmv.com>
2017-09-06 19:20:14 +0200

    New folder introduced under the examples folder. Here it will be developed the new example of the iop using canbus.

---
+++ xky/examples/iop_can/iop/iop_config.c
@@ -0,0 +1,68 @@
+#include <iop.h>
+
+/**
+ * @brief IOP remote ports
+ */
+extern iop_port_t remote_ports[3];
+
+/**
+ * @brief Logical devices
+ */
+extern iop_logical_device_t logical_device_0;
+extern iop_logical_device_t logical_device_1;
+
+/**
+ * @brief Physical devices
+ */
+extern iop_physical_device_t physical_device_0;
+
+/**
+ * @brief Physical devices list
+ */
+static iop_physical_device_t *physical_device_list[1] = \
+{
+    &physical_device_0,
+};
+
+/**
+ * @brief Logical devices list
+ */
+static iop_logical_device_t *logical_device_list[2] = \
+{
+    &logical_device_0,
+    &logical_device_1,
+};
+
+static iop_wrapper_t requests_storage[1024];
+
+/**
+ * @brief IOP buffers
+ */
+static iop_buffer_t iop_buffers[1024];
+static uint8_t iop_buffers_storage[1024 * IOP_BUFFER_SIZE];
+
+
+/**
+ * @brief IOP application configurations
+ */
+iop_configuration_t usr_configuration = {
+
+    .wrappers               = requests_storage,
+    .iop_buffers            = iop_buffers,
+    .iop_buffers_storage    = iop_buffers_storage,
+    .wrappers_count           = 1024,
+
+    .physical_devices       = {
+        .length             = 1,
+        .elements           = physical_device_list
+     },
+    .logical_devices       = {
+        .length             = 2,
+        .elements           = logical_device_list
+     },
+    .remote_ports           = {
+        .length             = 3,
+        .elements           = remote_ports
+    }
+};
+
