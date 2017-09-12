2d0b73169a207b27dc82c5d26229134799bc99e2
Guilherme Sanches <guilherme.sanches@gmv.com>
2017-09-12 11:39:13 +0200

    Intermidiate commit with work in progress. Starting to write functions to work with the new occan driver

---
+++ xky/libs/libiop/can_support.c
@@ -0,0 +1,21 @@
+/*
+ * can_support.c
+ *
+ *  Created on: Sep 7, 2017
+ *      Author: gmvs@gmv.com
+ */
+
+#include <can_support.h>
+
+
+void can_copy_header() {
+
+}
+
+void can_compared_header(){
+
+}
+
+void can_compare_id(){
+
+}
