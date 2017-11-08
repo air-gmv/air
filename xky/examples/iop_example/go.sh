e4a07f3d2d410e9845f51e45b5e5569a27f7713e
Guilherme Sanches <guilherme.sanches@gmv.com>
2017-11-08 16:28:22 +0100

    Changes to the go.sh

--- xky/examples/iop_example/go.sh
+++ xky/examples/iop_example/go.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 make clean
 make
-echo Copying files to remote computer...
-sshpass -p "$(AIR_RUNNER_PASSWORD)" scp executable/XKYAPP.exe $(AIR_RUNNER_USER)@$(AIR_RUNNER_IP):~/xky/examples/iop_example/executable/IOP_gmvs.exe
-echo Files copied
+sparc-rtems-objdump -d iop/p0.exe > iop/p0.txt
+sparc-rtems-objdump -d sampling/p1.exe > sampling/p1.txt
+sparc-rtems-objdump -d queuing/p2.exe > queuing/p2.txt
+sparc-rtems-objdump -d executable/XKYAPP.exe > executable/XKYAPP.txt
+sparc-rtems-nm executable/XKYAPP.exe > executable/XKYAPP.num
+
+figlet "Copying files to remote computer..."
+read -s -p "Enter password: " password
+sshpass -p password scp executable/XKYAPP.exe gmvs@$(AIR_RUNNER_IP):~/executable/XKYAPP.exe
+sshpass -p password scp iop/p0.txt.exe gmvs@$(AIR_RUNNER_IP):~/executable/p0.txt
+sshpass -p password scp sampling/p1.txt gmvs@$(AIR_RUNNER_IP):~/executable/p1.txt
+sshpass -p password scp queuing/p2.txt gmvs@$(AIR_RUNNER_IP):~/executable/p2.txt
+sshpass -p password scp executable/XKYAPP.num gmvs@$(AIR_RUNNER_IP):~/executable/XKYAPP.num
+figlet "Files copied"
