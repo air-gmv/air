9d78d5026c5d6454682f7156572fe1240a8042ac
Guilherme Sanches <guilherme.sanches@gmv.com>
2017-11-15 14:44:10 +0100

    Changes in occan upper layer
    The can read/write tasks have been completed. The can_header_compare has
    also been completed and it's no longer a stub

--- xky/libs/libiop/can_support.c
+++ xky/libs/libiop/can_support.c
@@ -19,10 +19,18 @@ void can_copy_header(
 
 }
 
-void can_compare_header(
+uint16_t can_compare_header(
 		iop_wrapper_t *wrapper,
 		iop_header_t *header){
 
+	can_header_t *can_header = (can_header_t *) header;
+	int id = *(int *) (wrapper->buffer->v_addr +
+			wrapper->buffer->header_off +
+			3*sizeof(int));
+	if (id == can_header->id){
+		return 1;
+	}
+	return 0;
 }
 
 void can_prebuild_header(
