5ecef450fb9059e4cf386595cf9e4df65bfd0890
Daniel Silveira <dtms@gmv.com>
2018-01-02 20:09:25 +0100

    New configurator version 3.6 With error log for debugging It generates a configurator log

--- xky/tools/configurator/configurator.py
+++ xky/tools/configurator/configurator.py
@@ -13,6 +13,7 @@ import xky.configurations as xky_configuration
 import utils.terminal as terminal_utils
 import tools.configure_xky as configure_xky
 import tools.configure_app as configure_app
+import logging
 
 from sys import exit
 from utils.logger import Logger
@@ -21,17 +22,22 @@ from utils.logger import Logger
 from localization.common import *
 from argparse import  ArgumentParser,  RawTextHelpFormatter
 
-__version__ 	= '3.5'
-__copyright__ 	= 'Copyright (C) GMVIS Skysoft S.A., 2014'
+__version__ 	= '3.6'
+__copyright__ 	= 'Copyright (C) GMVIS Skysoft S.A., 2018'
 __author__ 		= 'pfnf'
 __app__			= os.path.basename(__file__)
+
+#Uncomment this for full debugging of an error and check configurator.log results
+logging.basicConfig(filename='configurator.log', format='%(levelname)s:%(filename)s:%(funcName)s:%(lineno)d:%(message)s', filemode='w', level=logging.DEBUG)
+# we can set more attributes to log see https://docs.python.org/2/library/logging.html#logrecord-attributes
+logging.info('Configurator log activated')
 
 ##
 # @brief Configurator entry point
 if __name__ == "__main__":
 
     # start event logger
-    logger = Logger()
+    logger = logging.getLogger('air_configurator');
 
     # display
     terminal_utils.printWelcomeBox(AIR_CONFIG_WELCOME, __version__)
@@ -46,15 +52,18 @@ if __name__ == "__main__":
 
     # check if we are configuring XKY or a partition
     if xky.WORKING_DIRECTORY == xky.ROOT_DIRECTORY:
-
+        logging.info('Running AIR configuration including RTEMS activated')
         args = configure_xky.InputArgs(arg_parser, logger)
         configure_xky.Run(args, logger)
 
     else:
 
         # load OS configurations
+        logging.info('Running AIR partition configuration')
         os_configuration = xky_configuration.load_configuration(logger)
-        if os_configuration is None: exit(-1)
+        if os_configuration is None: 
+            logging.error ('AIR is not configured aborting')
+            exit(-1)
 
         # run the application configuration
         configure_app.InputArgs(arg_parser, logger)
