14e743034c5aa2b5a47149853c5db48395909082
Daniel Silveira <dtms@gmv.com>
2018-01-08 09:46:38 +0100

    Removed from repository the go.sh scripts
    This scripts may contain our passwords, removed for safety

--- xky/examples/iop_example/go.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-make clean
-make
-echo Copying files to remote computer...
-sshpass -p "latitudee5440" scp executable/XKYAPP.exe ngmp@$(AIR_RUNNER_IP):~/xky/examples/iop_example/executable/IOP_gmvs.exe
-echo Files copied
