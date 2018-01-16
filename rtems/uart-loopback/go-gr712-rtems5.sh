7a9613b460d27b9c68f08369aaeb77add622905e
Luca Giordano <lllg@gmv.es>
2018-01-16 18:37:54 +0100

    Add RTEMS examples
    	modified:   .gitignore
    	new file:   rtems/io_partition/IOPethernet.h
    	new file:   rtems/io_partition/IOPgreth.c
    	new file:   rtems/io_partition/IOPgreth.h
    	new file:   rtems/io_partition/IOPinterface.c
    	new file:   rtems/io_partition/IOPinterface.h
    	new file:   rtems/io_partition/IOPuip.c
    	new file:   rtems/io_partition/IOPuip.h
    	new file:   rtems/io_partition/Makefile-4.12
    	new file:   rtems/io_partition/Makefile-5
    	new file:   rtems/io_partition/_THIS_EXAMPLE_DOES_NOT_WORK_
    	new file:   rtems/io_partition/go-gr712-rtems5.sh
    	new file:   rtems/io_partition/go-gr712rc.sh
    	new file:   rtems/io_partition/go-leon3-rtems5.sh
    	new file:   rtems/io_partition/go-leon3.sh
    	new file:   rtems/io_partition/iop.c
    	new file:   rtems/io_partition/iop.h
    	new file:   rtems/io_partition/iop_lib.c
    	new file:   rtems/io_partition/partition_defs.h
    	new file:   rtems/occan/Makefile-4.12
    	new file:   rtems/occan/Makefile-5
    	new file:   rtems/occan/_THIS_EXAMPLE_DOES_NOT_WORK_
    	new file:   rtems/occan/config.c
    	new file:   rtems/occan/config_leon3_drvmgr.4.11
    	new file:   rtems/occan/config_leon3_drvmgr.c
    	new file:   rtems/occan/config_leon3_drvmgr.c.4.10
    	new file:   rtems/occan/config_leon4_n2x.c
    	new file:   rtems/occan/go-gr712-rtems5.sh
    	new file:   rtems/occan/go-gr712rc.sh
    	new file:   rtems/occan/go-leon3-rtems5.sh
    	new file:   rtems/occan/go-leon3.sh
    	new file:   rtems/occan/networkconfig.h
    	new file:   rtems/occan/occan_lib.c
    	new file:   rtems/occan/occan_lib.h
    	new file:   rtems/occan/po_hi_driver_leon_config.c
    	new file:   rtems/occan/rtems-occan.c
    	new file:   rtems/spw2boards/Makefile-4.12
    	new file:   rtems/spw2boards/Makefile-5
    	new file:   rtems/spw2boards/_THIS_EXAMPLE_DOES_NOT_WORK_
    	new file:   rtems/spw2boards/config.c
    	new file:   rtems/spw2boards/config_leon3_drvmgr.c
    	new file:   rtems/spw2boards/config_leon4_n2x.c
    	new file:   rtems/spw2boards/go-gr712-rtems5.sh
    	new file:   rtems/spw2boards/go-gr712rc.sh
    	new file:   rtems/spw2boards/go-leon3-rtems5.sh
    	new file:   rtems/spw2boards/go-leon3.sh
    	new file:   rtems/spw2boards/l.tar.gz
    	new file:   rtems/spw2boards/networkconfig.h
    	new file:   rtems/spw2boards/po_hi_driver_leon_config.c
    	new file:   rtems/spw2boards/rtems-spwtest-2boards.c
    	new file:   rtems/uart-loopback/Makefile-4.12
    	new file:   rtems/uart-loopback/Makefile-5
    	new file:   rtems/uart-loopback/_THIS_EXAMPLE_DOES_NOT_WORK_
    	new file:   rtems/uart-loopback/go-gr712-rtems5.sh
    	new file:   rtems/uart-loopback/go-gr712rc.sh
    	new file:   rtems/uart-loopback/go-leon3-rtems5.sh
    	new file:   rtems/uart-loopback/go-leon3.sh
    	new file:   rtems/uart-loopback/rtems-uart-loopback.c
    	new file:   rtems/uart-loopback/xxx/config.c
    	new file:   rtems/uart-loopback/xxx/config_leon3_drvmgr.4.11
    	new file:   rtems/uart-loopback/xxx/config_leon3_drvmgr.c
    	new file:   rtems/uart-loopback/xxx/config_leon3_drvmgr.c.4.10
    	new file:   rtems/uart-loopback/xxx/config_leon4_n2x.c
    	new file:   rtems/uart-loopback/xxx/networkconfig.h
    	new file:   rtems/uart-loopback/xxx/po_hi_driver_leon_config.c
    	new file:   rtems/workingttcp/Makefile-4.12
    	new file:   rtems/workingttcp/Makefile-5
    	new file:   rtems/workingttcp/_THIS_EXAMPLE_DOES_NOT_WORK_
    	new file:   rtems/workingttcp/config.c
    	new file:   rtems/workingttcp/config_leon3_drvmgr.c
    	new file:   rtems/workingttcp/go-gr712-rtems412.sh
    	new file:   rtems/workingttcp/go-gr712-rtems5.sh
    	new file:   rtems/workingttcp/go-leon3-rtems412.sh
    	new file:   rtems/workingttcp/go-leon3-rtems5.sh
    	new file:   rtems/workingttcp/networkconfig.h
    	new file:   rtems/workingttcp/rtems-ttcp.c
    	new file:   rtems/workingttcp/rtems-ttcp.h
    	new file:   rtems/workingttcp/ut/Makefile-5
    	new file:   rtems/workingttcp/ut/Makefile-gr712rc.ut
    	new file:   rtems/workingttcp/ut/Makefile-leon3.ut
    	new file:   rtems/workingttcp/ut/go-rtems5-sparc-leon3-ut.sh
    	new file:   rtems/workingttcp/ut/rtems-ttcp_ut.cc
    
    Refs: #118203

---
+++ rtems/uart-loopback/go-gr712-rtems5.sh
@@ -0,0 +1,3 @@
+rm -rf b-gr712rc/
+export RTEMS_MAKEFILE_PATH=/opt/rtems/5/sparc-rtems5/gr712rc
+make -f Makefile-5
---
+++ rtems/workingttcp/rtems-ttcp.h
@@ -0,0 +1,19 @@
+/* 
+ * rtems-ttcp include file
+ */
+
+#ifndef RTEMS_TTCP_H_
+#define RTEMS_TTCP_H_
+
+//
+// Forward declarations
+
+#ifdef GTEST /* LLLG XXX */
+int yadayadayada(int value);
+#endif /* GTEST LLLG XXX */
+
+void test_network(void);
+
+
+
+#endif  // RTEMS_TTCP_H_
