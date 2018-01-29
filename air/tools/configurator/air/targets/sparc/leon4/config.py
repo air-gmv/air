5e5b827eec9454a55f59c8200a6e2f82c2fb87e6
Daniel Silveira <dtms@gmv.com>
2018-01-29 13:40:35 +0100

    Removed RTEMS checkout feature
    It seems unecessary to do the git checkout of RTEMS and can cause git
    problems.
    
    Also added LDFLAGS to support prints in RTEMS

--- air/tools/configurator/air/targets/sparc/leon4/config.py
+++ air/tools/configurator/air/targets/sparc/leon4/config.py
@@ -32,7 +32,7 @@ kernel_compiler = dict(
     CFLAGS="",
     CPPFLAGS="-mcpu=leon3 -mflat -fno-builtin -nodefaultlibs -O2 -Wall",
     CXXFLAGS="",
-    LDFLAGS="",
+    LDFLAGS="-Wl,--gc-sections -Wl,--wrap=printf -Wl,--wrap=puts -Wl,--wrap=putchar",
     ARFLAGS="ruv"
 )
 
@@ -46,7 +46,7 @@ kernel_compiler_no_fpu = dict(
 	CFLAGS="",
 	CPPFLAGS="-mcpu=leon3 -mflat -fno-builtin -nodefaultlibs -O2 -Wall",
 	CXXFLAGS="",
-	LDFLAGS="",
+	LDFLAGS="-Wl,--gc-sections -Wl,--wrap=printf -Wl,--wrap=puts -Wl,--wrap=putchar",
 	ARFLAGS="ruv"
 )
 
