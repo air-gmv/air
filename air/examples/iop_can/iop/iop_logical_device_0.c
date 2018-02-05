880c1ca0b712c995af45a52e731669e2c3d09bf2
Guilherme Sanches <guilherme.sanches@gmv.com>
2018-02-05 19:24:14 +0100

    Changes to try solve the makefile.inc problem. rtems48i is being compiled by sparc-rtems5-gcc when it can only be compiled by the sparc-rtems4.11-gcc at the most.
    refs #129490
    
     Changes to be committed:
    	new file:   examples/iop_can/config.xml
    	new file:   examples/iop_can/iop.xml
    	renamed:    examples/iop_example/iop/iop_config.c -> examples/iop_can/iop/iop_config.c
    	renamed:    examples/iop_example/iop/iop_logical_device_0.c -> examples/iop_can/iop/iop_logical_device_0.c
    	renamed:    examples/iop_example/iop/iop_physical_device_0.c -> examples/iop_can/iop/iop_physical_device_0.c
    	renamed:    examples/iop_example/iop/iop_ports.c -> examples/iop_can/iop/iop_ports.c
    	new file:   examples/iop_can/queuing/queuing.c
    	new file:   examples/iop_can/sampling/sampling.c
    	new file:   libs/libiop/can_support.c
    	new file:   libs/libiop/can_tasks.c
    	new file:   libs/libiop/drivers/occan/include/grlib.h
    	new file:   libs/libiop/drivers/occan/include/iop_occan.h
    	new file:   libs/libiop/drivers/occan/include/occan_msg_queue.h
    	new file:   libs/libiop/drivers/occan/iop_occan.c
    	new file:   libs/libiop/drivers/occan/occan_msg_queue.c
    	modified:   libs/libiop/eth_tasks.c
    	new file:   libs/libiop/include/can_support.h
    	modified:   libs/libiop/include/iop.h
    	modified:   libs/libiop/include/iop_headers.h
    	modified:   pmk/core/ports.c
    	modified:   pos/rtems48i/edilib/Makefile.hc
    	modified:   pos/rtems48i/rtems48i/c/src/lib/libbsp/sparc/shared/amba/ambapp.c
    	modified:   pos/rtems48i/rtems48i/c/src/lib/libbsp/sparc/shared/include/ambapp.h
    	modified:   pos/rtems5/rtems5/c/src/lib/libbsp/sparc/shared/amba/ambapp.c
    	modified:   tools/configurator/air/pos/rtems48i/rtems48i_makefile.mako
    	modified:   tools/configurator/air/pos/rtems48i/rtems48i_partition_makefile.mako
    	modified:   tools/configurator/air/targets/sparc/leon3/config.py
    	modified:   tools/configurator/air/targets/sparc/leon4/config.py
    	modified:   tools/configurator/parsers/iop/definitions.py
    	modified:   tools/configurator/parsers/iop/devices.py
    	modified:   tools/configurator/parsers/iop/gaisler_devices.py
    	modified:   tools/configurator/parsers/iop/parser.py
    	modified:   tools/configurator/parsers/iop/ti_devices.py
    	modified:   tools/configurator/templates/air_configuration/air_makefile.mako
    	modified:   tools/configurator/templates/app_configuration/iop/include/iop_templates.mako
    	modified:   tools/configurator/templates/app_configuration/iop/iop_grspw.mako
    	modified:   tools/configurator/templates/app_configuration/iop/iop_logical_device.mako
    	new file:   tools/configurator/templates/app_configuration/iop/iop_occan.mako
    	modified:   tools/configurator/templates/include/makefile.mako
    	modified:   tools/configurator/tools/configure_air.py
    	modified:   ../utils/jenkins/aftercommitbuild.sh

--- air/examples/iop_can/iop/iop_logical_device_0.c
+++ air/examples/iop_can/iop/iop_logical_device_0.c
@@ -11,40 +11,38 @@ extern iop_physical_device_t physical_device_0;
  */
 static iop_header_t route_header[1] = \
 {
-    {
-        .eth_header = {
-            .dst_ip      = { 192,168,0,3},
-            .dst_mac     = { 0x00,0x50,0xbf,0x50,0x07,0x0d},
-            .dst_port    = HTONS(13000),
-            .src_port    = HTONS(13000)
-        }
-    }
+	{
+		.can_header = {
+			.extended 	= 0,
+			.rtr 		= 0,
+			.sshot 		= 0,
+			.id  		= 2
+		}
+	}
 };
 
 /**
  * @brief Routes Scheduling
  */
 static uint32_t route_schedule_0[1] = \
-    { 1 };
-
-
+		{ 1 };
 
 /**
  * @brief Routes Configuration
  */
 static iop_logical_route_t logical_routes[1] = \
 {
-    {
-        .schedule   = route_schedule_0,
-        .header     = &route_header[0],
-        .device     = &physical_device_0
-    }
+	{
+		.schedule = route_schedule_0,
+		.header = &route_header[0],
+		.device = &physical_device_0
+	}
 };
 
 /**
  * @brief Logical Device configuration
  */
-iop_logical_device_t logical_device_0 = \
+iop_logical_device_t logical_device_0= \
 {
     .routes = {
         .length     = 1,
