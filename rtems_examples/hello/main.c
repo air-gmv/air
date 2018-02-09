27c2513fa0aff6b136bd39db15dc5d6153dc6ee4
Daniel Silveira <dtms@gmv.com>
2018-02-09 18:28:54 +0100

    Add pure RTEMS hello world example
    Usefull for training and comparison with AIR
    
    refs #112672

---
+++ rtems_examples/hello/main.c
@@ -0,0 +1,30 @@
+/*  Main
+ *
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id: init.c,v 1.13 2003/09/04 18:53:41 joel Exp $
+ */
+
+#include <stdio.h>
+
+int andp=0;
+
+
+void entry_point(void) {
+
+while(1)
+{
+  andp=andp+1;
+  printf( "*** RTEMS HELLO WORLD TEST **********\n" );
+  rtems_task_wake_after(5);
+
+
+}
+//  exit( 0 );
+
+}
