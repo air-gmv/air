5302bed433ab88aa867eabd8835de2919253e649
Luca Giordano <lllg@gmv.es>
2018-04-26 13:12:17 +0200

    Code cleaning and organizing stuff
        code cleaning.
        move compilation variables from rtems examples Makefiles to
        respective compilation script.
    
    refs #112269
    
    	modified:   .gitignore
    	modified:   air/libs/libiop/eth_tasks.c
    	modified:   rtems_examples/hello/Makefile-5
    	modified:   rtems_examples/hello/go-leon3-rtems5.sh
    	modified:   rtems_examples/hello/main.c
    	modified:   rtems_examples/workingttcp/Makefile-5
    	modified:   rtems_examples/workingttcp/go-gr712-rtems5.sh
    	modified:   rtems_examples/workingttcp/go-gr740-rtems5.sh
    	modified:   rtems_examples/workingttcp/go-leon3-rtems5.sh

--- rtems_examples/hello/go-leon3-rtems5.sh
+++ rtems_examples/hello/go-leon3-rtems5.sh
@@ -1,3 +1,6 @@
+export RTEMS_API=5
+export RTEMS_BSP=leon3
+
 rm -rf b-leon3/
 export RTEMS_MAKEFILE_PATH=/opt/rtems/5/sparc-rtems5/leon3
 make -f Makefile-5
@@ -7,4 +10,4 @@ sshpass -p "latitudee5440" scp l.tar.gz ngmp@83.240.244.231:~/drivers
 
 #sshpass -p "latitudee5440" scp o-optimize/ethgmv.exe ngmp@83.240.244.231:~/drivers/workingttcp.exe
 
-sshpass -p "cgd17832201" scp b-leon3/init.exe dtms@83.240.244.237:~/examples/air5/RTEMS_APP.exe
+sshpass -p "cgd17832201" scp b-leon3/hello.exe dtms@83.240.244.237:~/examples/air5/RTEMS_APP.exe
--- rtems_examples/workingttcp/go-gr712-rtems5.sh
+++ rtems_examples/workingttcp/go-gr712-rtems5.sh
@@ -1,5 +1,7 @@
 rm -rf b-gr712rc/
 export RTEMS_MAKEFILE_PATH=/opt/rtems/5/sparc-rtems5/gr712rc
+export RTEMS_API=5
+export RTEMS_BSP=gr712rc
 make -f Makefile-5
 #cp o-optimize/ethgmv.exe /home/assert/Desktop/C_DRIVE/vmshare/
 #tar -zcvf l.tar.gz o-optimize/ethgmv.exe 
