a4728418327ea9c9087c4e28f81e1177fc8998f0
Bruno Gomes <bmgomes@gmv.com>
2018-06-08 11:04:51 +0200

    RTEMS5 gr1553 example: first iteration torwards a simpler example customized to our testbench (SSF setup)
    refs #112306

--- rtems_examples/gr1553/rtems-gr1553bcbm.c
+++ rtems_examples/gr1553/rtems-gr1553bcbm.c
@@ -35,14 +35,6 @@
  *
  *  The BC processing loop prints out the current Major, Minor Frame and 
  *  Message Slot the BC is executing (INDICATION).
- *
- *  Bus Monitor (BM) example
- *  ------------------------
- *  Logger of 1553 bus, does not filter out anything, reads from a DMA area
- *  and compresses it into a larger log area. The larger log area can be read
- *  from a TCP/IP service using the linux_client.c example. The Linux
- *  application output it into a textfile for later processing.
- *
  */
 
 #include <rtems.h>
@@ -66,45 +58,11 @@
  */
 /*#define SOFT_EXTTRIG_ENABLE*/
 
-/* CONFIG: Define TIME_SYNC_MANAGEMENT to enable time synchronization to/from
- *         SpaceWire or TimeWire. The SPWCUC and GRCTM cores are used to
- *         sync the nodes. Use TIME_SYNC_MASTER to control if Slave or Master.
- */
-/*#define TIME_SYNC_MANAGEMENT */
-
-
-/* CONFIG: If TIME_SYNC_MANAGEMENT is defined this options has an effect. The
- *         TIME_SYNC_MASTER option controls whether time is received (SLAVE)
- *         or is distibuted (MASTER) to other time-slaves.
- */
-/*#define TIME_SYNC_MASTER */
-
-/* CONFIG: If TIME_SYNC_MANAGEMENT is defined this options has an effect.
- *         The TIME_SYNC_METHOD define determine which interface will be used
- *         for synchronization between Slave/Master.
- *
- *          0: TimeWire
- *          1: SpaceWire (Default)
- */
-#define TIME_SYNC_METHOD 1
-
-/* CONFIG: If TIME_SYNC_MANAGEMENT is defined this options has an effect. The
- *         TIME_GRSPW_DEVNAME option selects which SpaceWire core is used for
- *         sending time packets.
- *
- * Below is a defualt configuration for LEON2 and GR-RASTA-IO, and LEON3
- * with on-chip GRSPW.
- */
-#ifdef LEON2
-  #define TIME_GRSPW_DEVNAME "/dev/rastaio0/grspw0"
-#else
-  #define TIME_GRSPW_DEVNAME "/dev/grspw0"
-#endif
 
 /* CONFIG: If PRINT_DEVICE_TOPOLOGY is defined the example will print all devices
  *         found by the driver manager, and which bus they are situated on.
  */
-#define PRINT_DEVICE_TOPOLOGY
+/*#define PRINT_DEVICE_TOPOLOGY*/
 
 /* CONFIG: If PRINT_PCI_BUS is defined the example will print PCI configuration
  *         space after being setup by configuration library.
@@ -133,21 +91,14 @@
   /* Use 0x40000000 at the PCI board as base for the descriptor table */
   #define BD_TABLE_BASE TRANSLATE(0x40000000)
 
-  /* Bus Monitor (BM) LOGGING BASE ADDRESS : In SRAM of GR-RASTA-XXXX */
-  #define BM_LOG_BASE (0x40010000 | 1)
 #else
   /* No translation of Data buffers needed */
   #define TRANSLATE(adr) adr
   /* Let driver dynamically allocate the descriptor table */
   #define BD_TABLE_BASE NULL
 
-  /* Bus Monitor (BM) LOGGING BASE ADDRESS : Dynamically allocated */
-  #define BM_LOG_BASE NULL
 #endif
 
-#ifndef TIME_SYNC_MANAGEMENT
-  #undef TIME_SYNC_MASTER
-#endif
 
 
 
@@ -167,11 +118,6 @@ rtems_task Init( rtems_task_argument argument);	/* forward declaration needed */
 
 #include <rtems/confdefs.h>
 
-/* Include BM Log application configuration. We need to know if
- * the ethernet server is to be started, and if the log is to be
- * compressed.
- */
-#include "config_bm.h"
 
 /* Configure Driver manager */
 #if defined(RTEMS_DRVMGR_STARTUP) && defined(LEON3) /* if --drvmgr was given to configure */
@@ -183,29 +129,14 @@ rtems_task Init( rtems_task_argument argument);	/* forward declaration needed */
   #define CONFIGURE_DRIVER_AMBAPP_GAISLER_APBUART
  #endif
 #endif
-#ifdef ETH_SERVER
-#define CONFIGURE_DRIVER_AMBAPP_GAISLER_GRETH
-#endif
-//#define CONFIGURE_DRIVER_PCI_GR_RASTA_IO        /* GR-RASTA-IO PCI Target Driver */
-
-#ifdef TIME_SYNC_MANAGEMENT
-  /* Add the GRSPW driver when so that time packets can be sent/received */
-  #define CONFIGURE_DRIVER_AMBAPP_GAISLER_GRSPW
-#endif
+//#define CONFIGURE_DRIVER_PCI_GR_RASTA_IO        /* GR-RASTA-IO PCI Target Driver */s
 
 /* CONFIGURE GR-RASTA-IO Board */
 #define CONFIGURE_DRIVER_AMBAPP_MCTRL           /* Driver for Memory controller needed when using SRAM on PCI board */
 #define RASTA_IO_SRAM
 
-/******** ADD BC AND BM DRIVERS **********/
+/******** ADD BC DRIVERS **********/
 #define CONFIGURE_DRIVER_AMBAPP_GAISLER_GR1553BC
-#define CONFIGURE_DRIVER_AMBAPP_GAISLER_GR1553BM
-
-#ifdef TIME_SYNC_MANAGEMENT
-/******** ADD SPWCUC AND GRCTM DRIVERS **********/
-#define CONFIGURE_DRIVER_AMBAPP_GAISLER_SPWCUC
-#define CONFIGURE_DRIVER_AMBAPP_GAISLER_GRCTM
-#endif
 
 #ifdef LEON2
   /* PCI support for AT697 */
@@ -219,43 +150,17 @@ rtems_task Init( rtems_task_argument argument);	/* forward declaration needed */
 #include <stdio.h>
 #include <stdlib.h>
 
-/* If Ethernet server enable networking */
-#ifdef ETH_SERVER
-#define ENABLE_NETWORK
-#else
-#undef ENABLE_NETWORK
-#endif
-
-#undef ENABLE_NETWORK_SMC_LEON3
+//#undef ENABLE_NETWORK_SMC_LEON3
 
 #include "config.c"
 
 int init_bc_list(void);
 int init_bc(void);
-int init_bm(void);
-int bm_log(void);
-
-int init_spwcuc(int tx, char *grspw_devname);
-void spwcuc_handling(void);
-
-int init_ctm(int tx, int spw);
-void ctm_handling(void);
-
-/* Ethernet Server functions */
-extern int server_init(char *host, int port);
-extern int server_wait_client(void);
-extern int server_loop(void);
-extern void server_stop();
-volatile int client_avail = 0;
-
-rtems_id taskEthid;
-rtems_name taskEthname;
 
 rtems_task Init(
   rtems_task_argument ignored
 )
 {
-	int status;
 
 	/* Initialize Driver manager and Networking, in config.c */
 	system_init();
@@ -281,40 +186,9 @@ rtems_task Init(
 	ambapp_print(ambapp_root, 10);
 #endif
 
-	if ( (status=init_bm()) ) {
-		printf("Failed to initialize BM: %d\n", status);
-		exit(0);
-	}
-
-#if defined(TIME_SYNC_MANAGEMENT) && (TIME_SYNC_METHOD == 1)
-#ifdef TIME_SYNC_MASTER
-	if ( (status=init_spwcuc(1, TIME_GRSPW_DEVNAME)) ) {
-		printf("TIME: TX\n");
-#else
-	if ( (status=init_spwcuc(0, TIME_GRSPW_DEVNAME)) ) {
-		printf("TIME: RX\n");
-#endif
-		printf("Failed to initialize SPWCUC TIME handling : %d\n", status);
-		exit(0);
-	}
-#endif
-
-#ifdef TIME_SYNC_MANAGEMENT
-#ifdef TIME_SYNC_MASTER
-	if ( init_ctm(1, TIME_SYNC_METHOD) ) {
-#else
-	if ( init_ctm(0, TIME_SYNC_METHOD) ) {
-#endif
-		printf("Failed to initialize CTM handling\n");
-		exit(0);
-	}
-#endif
-
 	if ( init_bc() ) {
 		printf("Failed to initialize BC\n");
-#ifdef ETH_SERVER
-		 server_stop();
-#endif
+
 		exit(0);
 	}
 
@@ -404,16 +278,6 @@ int init_bc(void)
 		 *  All   - .         - Time Handling
 		 */
 
-#ifdef TIME_SYNC_MANAGEMENT
-#if (TIME_SYNC_METHOD == 1)
-		/* Handle SPWCUC Time Stuff */
-		spwcuc_handling();
-#endif
-
-		/* Handle CTM Stuff */
-		ctm_handling();
-#endif
-
 		/* Execute every 10 ticks
 		 * 
 		 */
@@ -431,14 +295,6 @@ int init_bc(void)
 
 			/* Talk to RTs */
 			bc_list_process();
-		}
-
-		/* Handle Log buffer */
-		if ( cnt == 1 ) {
-			if ( bm_log() ) {
-				printf("BM Log failed\n");
-				return -2;
-			}
 		}
 
 		/* Execute every 5 ticks  */
@@ -476,5 +332,3 @@ void bc_enter_com_state(unsigned int rts_up)
 	}
 	
 }
-
-#include "bm_logger.c"
