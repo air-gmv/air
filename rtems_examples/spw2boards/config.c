79d8710b7f1ca99007b14aa6be099876c2c63279
Daniel Silveira <dtms@gmv.com>
2018-06-18 10:41:59 +0200

    RTEMS Spw example build for gr740
    refs #78443

--- rtems_examples/spw2boards/config.c
+++ rtems_examples/spw2boards/config.c
@@ -15,39 +15,48 @@
 /*#define LEON2_GRLIB*/
 
 /* Configure Network if enabled */
-#include <bsp/network_interface_add.h>
+#include <bsp.h>
+#include <rtems.h>
+#include <rtems/rtems_bsdnet.h>
+
+#include <network_interface_add.h>
+
 #include "networkconfig.h"
 
 #include "config_leon3_drvmgr.c"
-//#include "config_leon4_n2x.c"
 
 void system_init(void)
 {
-
-
-	/* Initializing Driver Manager if not alread performed by BSP */
-	system_init2();
-	printf("Initializing manager Config\n");
-	if ( drvmgr_init() ) {
-		printf("Driver manager Failed to initialize\n");
-		exit(-1);
-	}
-
-
-	///* Print Driver manager drivers and their assigned devices
-//	 drvmgr_summary();
-//	 drvmgr_info_drvs(OPTION_INFO_ALL);
-	  //     drvmgr_print_devs(0xfffff);
-	// */
+printk(">>system_init2()\n");
 #ifdef CONFIGURE_DRIVER_AMBAPP_GAISLER_GRETH
-
-//#ifdef TURNOFF
-	/* Init network */
-	printf("Initializing network\n");
-	rtems_bsdnet_initialize_network ();
-	printf("Initializing network DONE\n\n");
-	rtems_bsdnet_show_inet_routes();
-	printf("\n");
-	rtems_bsdnet_show_if_stats();
+    /* Initializing Driver Manager if not already performed by BSP */
+    system_init2();
+    printk("Initializing manager Config\n");
+    if (drvmgr_init())
+    {
+        printk("Driver manager Failed to initialize\n");
+        exit(-1);
+    }
 #endif
+
+    ///* Print Driver manager drivers and their assigned devices
+printk(">>system_init(alpha)\n");
+    drvmgr_summary();
+printk(">>system_init(bravo)\n");
+    drvmgr_info_drvs(OPTION_INFO_ALL);
+printk(">>system_init(charlie)\n");
+    drvmgr_print_devs(0xfffff);
+printk(">>system_init(delta)\n");
+    drvmgr_print_topo();
+    // */
+
+    //#ifdef TURNOFF
+    /* Init network */
+    printk("Initializing network\n");
+    rtems_bsdnet_initialize_network ();
+    printk("Initializing network DONE\n\n");
+    rtems_bsdnet_show_inet_routes();
+    printk("\n");
+    rtems_bsdnet_show_if_stats();
+    //#endif
 }
