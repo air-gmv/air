c84c33ef43e8207a9f45f05bc8cbd843ce440b9c
trjfra <jfrosa@gmv.com>
2018-08-22 11:42:05 +0100

    Addition of InterPartComm TASTE with AIR example
    refs #112300

---
+++ taste_examples/InterPartComm/hello/hello.h
@@ -0,0 +1,23 @@
+/* This file was generated automatically: DO NOT MODIFY IT ! */
+
+/* Declaration of the functions that have to be provided by the user */
+
+#ifndef __USER_CODE_H_hello__
+#define __USER_CODE_H_hello__
+
+#include "C_ASN1_Types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void hello_startup();
+
+void hello_PI_s(const asn1SccTASTE_Boolean *);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
