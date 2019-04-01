2ce8c8e5aa0ededb762928ac4e433aff5f1768fe
Bruno Gomes <bmgomes@gmv.com>
2019-04-01 13:06:54 +0200

    AIR HM: improve HM example
    refs #186450

--- air/examples/hm/p0/p0main.c
+++ air/examples/hm/p0/p0main.c
@@ -8,28 +8,56 @@
  * ==========================================================================*/
 
 #include <air.h>
+#include <rtems.h>
 #ifdef RTEMS48I
-	#include <pprintf.h>
+    #include <pprintf.h>
+#else
+    #include <stdio.h>
 #endif
 
 int producer() {
-	
-	for (;;)
-	{
+
+    long pid;
+    air_status_code_e rc;
+    air_partition_status_t status;
+    int x = 2;
+    volatile int y = 0;
+
+    rc = air_syscall_get_partition_id(NULL, &pid);
+    if(AIR_NO_ERROR != rc)
+    {
+        printf("P0main air_syscall_get_partition_id ERROR: %u\n", rc);
+    }
+
+    rc = air_syscall_get_partition_status(pid, &status);
+    if(AIR_NO_ERROR != rc)
+    {
+        printf("P0main air_syscall_get_partition_status ERROR: %u\n", rc);
+    }
+
+    /*Example on how to act differently if a HM restart action was selected*/
+    if(status.start_condition != AIR_START_CONDITION_HM_PARTITION_RESTART)
+    {
+        /*Force a division by 0*/
+        x = x/y;
+    }
+
+    uint32_t tps = 1000000 / air_syscall_get_us_per_tick();
+
+    for (;;)
+    {
       #ifdef RTEMS48I
-        pprintf("In partition PO loop t20\n");
+        pprintf("In partition PO loop\n");
       #else
-        printf("In partition PO loop t20\n");
+        printf("In partition PO loop\n");
       #endif
-	    rtems_task_wake_after(20);
+        rtems_task_wake_after(tps/2);
       #ifdef RTEMS48I
-        pprintf("P0 : Waking up\n");
+        pprintf("P0 : Waking up %d\n", x);
       #else
-  	    printf("P0 : Waking up\n");
+        printf("P0 : Waking up %d\n", x);
       #endif
-	}
-	
-	return AIR_SUCCESSFUL;
+    }
+
+    return AIR_SUCCESSFUL;
 }
-
-
--- air/examples/hm/p1/p1main.c
+++ air/examples/hm/p1/p1main.c
@@ -8,26 +8,44 @@
  * ==========================================================================*/
 
 #include <air.h>
+#include <rtems.h>
 #ifdef RTEMS48I
-	#include <pprintf.h>
+    #include <pprintf.h>
+#else
+    #include <stdio.h>
 #endif
 
+void partition_HM_callback(air_state_e state_id,air_error_e error_id)
+{
+    printf("ENTERING partition_HM_callback\tSTATE:%d\tERROR:%d\n",state_id,error_id);
+
+    /*implement some appropriate handler here*/
+}
+
 int receiver() {
-	
-	for (;;)
-	{
+
+    /*Force a division by 0*/
+    int x = 2;
+    volatile int y = 0;
+
+    x = x/y;
+
+    uint32_t tps = 1000000 / air_syscall_get_us_per_tick();
+
+    for (;;)
+    {
       #ifdef RTEMS48I
-        pprintf("In partition P1 loop t20\n");
+        pprintf("In partition P1 loop\n");
       #else
-        printf("In partition P1 loop t20\n");
+        printf("In partition P1 loop\n");
       #endif
-	    rtems_task_wake_after(20);
+        rtems_task_wake_after(tps/2);
       #ifdef RTEMS48I
-        pprintf("P1 : Waking up\n");
+        pprintf("P1 : Waking up - %d\n", x);
       #else
-  	    printf("P1 : Waking up\n");
+        printf("P1 : Waking up - %d\n", x);
       #endif
-	}
-	
-	return AIR_SUCCESSFUL;
+    }
+
+    return AIR_SUCCESSFUL;
 }
