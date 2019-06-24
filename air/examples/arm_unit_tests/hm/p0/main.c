ee17f814f7622b18f908d99a2236e5936b3b1180
Luis Murta <lmurta@gmv.com>
2019-06-24 16:21:44 +0100

    Updates examples for further testing
    Modifies the HM, single and time examples
    Adds the miura config.xml
    
    refs #134456
    
    Changes to be committed:
    	modified:   air/examples/arm_unit_tests/hm/p0/main.c
    	new file:   air/examples/arm_unit_tests/miura/config.xml
    	modified:   air/examples/arm_unit_tests/single/config.xml
    	modified:   air/examples/arm_unit_tests/time/config.xml

--- air/examples/arm_unit_tests/hm/p0/main.c
+++ air/examples/arm_unit_tests/hm/p0/main.c
@@ -15,10 +15,10 @@ char message[] = "I am here at time: ";
  */
 void entry_point() {
 
-	air_u64_t us_per_tick = air_syscall_get_us_per_tick();
+	air_u32_t us_per_tick = air_syscall_get_us_per_tick();
 
 	pprintf("P0:: us_per_tick = %ld us\n", us_per_tick);
-	air_u32_t ms_per_tick = (air_u32_t)((air_u32_t)us_per_tick/1000);
+	air_u32_t ms_per_tick = (air_u32_t)us_per_tick/1000;
 	pprintf("P0:: ms_per_tick = %d ms\n", ms_per_tick);
 	int a = 0;
 	int i;
@@ -32,14 +32,12 @@ void entry_point() {
 	pprintf("P0:: I am here at partition %d. ms/tick = %d, *2 %d, +1 %d, /2 %d \n",
 			a, ms_per_tick, (ms_per_tick*2), (ms_per_tick+1), (ms_per_tick/2));
 
-	air_u64_t i_tick, i_time, tick, time, t_tick;
-	air_u64_t d_tick;
-	air_u64_t d_time;
+	air_u32_t i_tick, i_time, tick, time, t_tick;
+	air_u32_t d_tick;
+	air_u32_t d_time;
 
-	i_tick = air_syscall_get_elapsed_ticks();
-	i_time = (air_u64_t)(i_tick * ms_per_tick);
-
-	tick = (i_tick >> 32);
+	i_tick = (air_u32_t)air_syscall_get_elapsed_ticks();
+	i_time = (i_tick * ms_per_tick);
 
 	pprintf("P0::1 I am here at tick(time): %ld(%ldms)   %ld\n",
 			i_tick, i_time, tick);
@@ -52,20 +50,20 @@ void entry_point() {
 
 	double d1 = 3/2;
 
-	tick = air_syscall_get_elapsed_ticks();
-	time = (air_u64_t)(tick*ms_per_tick);
+	tick = (air_u32_t)air_syscall_get_elapsed_ticks();
+	time = (tick*ms_per_tick);
 	d_tick = (tick - i_tick);
-	d_time = (air_u64_t)(d_tick*ms_per_tick);
+	d_time = (d_tick*ms_per_tick);
 
 	pprintf("P0::2 /0 = %x, %d I am here at tick(time): %ld(%ldms)  after %ld(%ldms) passed\n",
 			(air_u32_t)zero, (air_u32_t)d1, tick, time, d_tick, d_time);
 
 	for(i = 0; i < 0x6000000; ++i);
 
-	t_tick = air_syscall_get_elapsed_ticks();
-	time = (air_u64_t)(t_tick*ms_per_tick);
+	t_tick = (air_u32_t)air_syscall_get_elapsed_ticks();
+	time = (t_tick*ms_per_tick);
 	d_tick = (t_tick - tick);
-	d_time = (air_u64_t)(d_tick*ms_per_tick);
+	d_time = (d_tick*ms_per_tick);
 
 	pprintf("P0::3 /0 = %x, %x I am here at tick(time): %ld(%ldms)  after %ld(%ldms) passed\n",
 			(air_u32_t)zero, (air_u32_t)d1, t_tick, time, d_tick, d_time);
