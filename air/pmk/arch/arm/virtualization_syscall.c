8e27bf4d1cd7b316ac784ee3fb670481c435563b
Luis Murta <lmurta@gmv.com>
2019-06-24 17:03:53 +0100

    Minor fix on the arch structures and rett syscall
    arm_virtual_gic_t reduced in size
    [ISSUE] further interrupt context save/restore needs to be done
    
    Adds field in arm_mmu_context_t for alignment and removes deprecated entry in arm_mmu_configuration_t
    
    AIR state field doesn't need to be updated in the rett system call since it is done in the remove_hm_event function
    
    refs #134456
    
    Changes to be committed:
    	modified:   air/pmk/arch/arm/include/armv7.h
    	modified:   air/pmk/arch/arm/virtualization_syscall.c

--- air/pmk/arch/arm/virtualization_syscall.c
+++ air/pmk/arch/arm/virtualization_syscall.c
@@ -76,7 +76,7 @@ void arm_syscall_set_psr(pmk_core_ctrl_t *core, air_u32_t val) {
 void arm_syscall_rett(pmk_core_ctrl_t *core) {
 
     core->context->vcpu.psr &= ~(ARM_PSR_A | ARM_PSR_I);
-    core->context->state = AIR_STATE_PARTITION_EXEC;
+//  core->context->state = AIR_STATE_PARTITION_EXEC; // its already in core_context_remove_hm_event
 }
 
 //air_u32_t arm_syscall_get_cache_register(void);
