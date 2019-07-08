96cd81016625d409dceee00d3d037494cce973ac
Luis Murta <lmurta@gmv.com>
2019-07-08 12:04:01 +0100

    Adds GIC and CPU context initialization attempt
    BSP adds GIC init function call and updates references to match architecture rename
    CPU context copied and from SPARC modified for ARM
    mmu.c includes updated references
    
    Adds asm_offsets.c stub
    
    Updates the config.py to include the new files
    
    refs #134456
    
    Changes to be committed:
    	new file:   air/pmk/arch/arm/asm_offsets.c
    	new file:   air/pmk/arch/arm/cpu.c
    	new file:   air/pmk/arch/arm/include/cpu.h
    	new file:   air/pmk/bsp/arm/shared/ic.c
    	new file:   air/pmk/bsp/arm/shared/include/ic.h
    	modified:   air/pmk/bsp/arm/zynq-z1/bsp.c
    	modified:   air/pmk/bsp/arm/zynq-z1/include/bsp.h
    	modified:   air/pmk/bsp/arm/zynq-z1/mmu.c
    	modified:   air/tools/configurator/air/targets/arm/zynq-z1/config.py
    
    [OC] 2018/04/27

--- air/pmk/bsp/arm/zynq-z1/bsp.c
+++ air/pmk/bsp/arm/zynq-z1/bsp.c
@@ -13,13 +13,13 @@
  */
 
 #include <bsp.h>
-#include <armcp15.h>
+#include <arm_cp15.h>
+#include <arm_a9mpcore.h>
 
-void bsp_start_hook(void *vector_table_begin)
-{
+void bsp_start_hook(void *vector_table_begin) {
     air_u32_t sctlr;
 
-    sctlr = get_system_control();
+    sctlr = cp15_get_system_control();
 
     /*
     * Current U-boot loader seems to start kernel image
@@ -33,14 +33,14 @@ void bsp_start_hook(void *vector_table_begin)
             * If the data cache is on then ensure that it is clean
             * before switching off to be extra carefull.
             */
-            data_cache_clean_all_levels();
+            cp15_data_cache_clean_all_levels();
         }
         instruction_synchronization_barrier();
         sctlr &= ~ ( CP15_CTRL_I | CP15_CTRL_C | CP15_CTRL_M | CP15_CTRL_A );
-        set_system_control(sctlr);
+        cp15_set_system_control(sctlr);
     }
 
-    instruction_cache_invalidate();
+    cp15_instruction_cache_invalidate();
 
     /*
     * The care should be taken there that no shared levels
@@ -50,28 +50,33 @@ void bsp_start_hook(void *vector_table_begin)
     * by CP15. So no special care to limit levels on the secondary
     * are required there. TODO understand
     */
-    data_cache_invalidate_all_levels();
-    branch_predictor_invalidate_all();
-    tlb_invalidate();
+    cp15_data_cache_invalidate_all_levels();
+    cp15_branch_predictor_invalidate_all();
+    cp15_tlb_invalidate();
 
-    /* If the vector table is already on the right memory position
-    * then no copy needs to be done. Otherwise ...
-    */
-    copy_vector_table();
+    air_u32_t cpu_id = cp15_get_multiprocessor_cpu_id();
 
-    clear_bss();
+    if(cpu_id == 0) {
+        /* If the vector table is already on the right memory position
+        * then no copy needs to be done. Otherwise ...
+        */
+        copy_vector_table();
+        clear_bss();
+    }
 }
 
 air_u32_t bsp_core_init() {
 
     a9mpcore_start_hook();
 
-    air_u32_t cpu_id = get_multiprocessor_cpu_id();
+    air_u32_t cpu_id = cp15_get_multiprocessor_cpu_id();
 
     if(cpu_id == 0) {
         start_global_timer();
         set_vector_base();
     }
+
+    gic_init(cpu_id);
 
     //TODO Do I need to copy from LoadMA to VMA
     // bsp_start_copy_sections();
--- air/pmk/bsp/arm/zynq-z1/include/bsp.h
+++ air/pmk/bsp/arm/zynq-z1/include/bsp.h
@@ -12,36 +12,81 @@
  * @brief BSP headers and inline assembly functions.
  */
 
+#ifndef ARM_BSP_H
+#define ARM_BSP_H
+
 #ifndef ASM
+
+#define SCU_BASE_MEMORY     0xf8f00000
+#define GT_BASE_MEMORY      0xf8f00200
+#define IC_CPU_BASE_MEMORY  0xf8f00100
+#define IC_DIST_BASE_MEMORY 0xf8f01000
 
 #include <air_arch.h>
 #include <armv7.h>
+#include <arm_cp15.h>
+#include <ic.h>
 
-#define SCU_BASE_MEMORY 0xf8f00000
-#define GT_BASE_MEMORY  0xf8f00200
+extern air_uptr_t bsp_section_bss_start;
+extern air_u32_t bsp_section_bss_size;
+extern air_uptr_t bsp_vector_table_begin;
+extern air_uptr_t bsp_vector_table_end;
+extern air_u32_t bsp_vector_table_size;
+extern air_uptr_t vector_table_begin;
 
-void bsp_start_hook(void);
+void bsp_start_hook(void *vector_table_begin);
 
-static inline clear_bss() {
-    memset(bsp_section_bss_start, 0, bsp_section_bss_size);
-//  air_i8_t *mem = (air_i8_t *)bsp_section_bss_start;
-//  for(air_u32_t i = 0; i < bsp_section_bss_size; i++) {
-//
-//      *mem = 0;
-//      mem++;
-//  }
+#define bsp_get_core_id() cp15_get_multiprocessor_cpu_id()
+#define bsp_get_number_of_cores() ic_processor_count()
+
+static inline void bsp_interrupt_core(air_u32_t core, air_u32_t dummy) {
+    if (bsp_get_core_id() == core) {
+        __asm__ volatile ("wfi");
+    }
 }
 
-static inline copy_vector_table() {
-    memcpy(bsp_vector_table_begin, vector_table_begin, 8);
-//  if (vector_table_begin != bsp_vector_table_begin) {
-//      air_i8_t *dst = (air_i8_t *) bsp_vector_table_begin;
-//      air_i8_t *src = (air_i8_t *) vector_table_begin;
-//
-//      for(air_u32_t i = 0; i < 8; i++) {
-//          *dst++ = *src++;
-//      }
-//  }
+static inline void bsp_interrupt_broadcast(air_u32_t dummy) {
+}
+
+static inline void clear_bss() {
+//  memset(bsp_section_bss_start, 0, bsp_section_bss_size);
+    air_i8_t *mem = (air_i8_t *)bsp_section_bss_start;
+    for(air_u32_t i = 0; i < bsp_section_bss_size; ++i) {
+        *mem++ = 0;
+    }
+}
+
+static inline void copy_vector_table() {
+//  memcpy(bsp_vector_table_begin, vector_table_begin, 8);
+    if (vector_table_begin != bsp_vector_table_begin) {
+       air_i8_t *dst = (air_i8_t *) bsp_vector_table_begin;
+       air_i8_t *src = (air_i8_t *) vector_table_begin;
+
+       for(air_u32_t i = 0; i < 8; i++) {
+           *dst++ = *src++;
+       }
+    }
+}
+
+static inline void set_vector_base(void) {
+    /* Do not use bsp_vector_table_begin == 0, since this will get optimized
+    * away. source: rtems source code
+    */
+    if (bsp_vector_table_end != bsp_vector_table_size) {
+        air_u32_t ctrl;
+
+        /* Assumes every core has Security Extensions */
+        cp15_set_vector_base_address((void *)bsp_vector_table_begin);
+
+        ctrl = cp15_get_system_control();
+        ctrl &= ~CP15_CTRL_V;
+        cp15_set_system_control(ctrl);
+    }
 }
 
 #endif /* ASM */
+
+#define BSP_IPC_IRQ 0 /* TODO for compatibility */
+#define BSP_IPC_PCS 0 /* TODO for compatibility */
+
+#endif /* ARM_BSP_H */
