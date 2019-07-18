cafcd3ff8304a18b06b830e056d723f593cd9782
Luis Murta <lmurta@gmv.com>
2019-07-18 15:12:05 +0100

    Updates the SVCs and stacks
    The SVCs now don't save context. The supervisor stack frame was defined in the armv7.h and added to the SVC handler. Most syscalls now defined, with some update in virtualization_syscall.c
    
    exception_default.S cleaned up and simple passing executing to default handler
    
    Stacks correctly defined in cpu.c and setup in start.S
    
    cpu.c also adds core_context_setup_partition, core_context_add_hm_event and core_context_remove_hm_event functions
    
    refs #134456
    
    Changes to be committed:
    	modified:   air/pmk/arch/arm/cpu.c
    	modified:   air/pmk/arch/arm/exception_default.S
    	modified:   air/pmk/arch/arm/exception_svc.S
    	modified:   air/pmk/arch/arm/exception_svc_old.S
    	modified:   air/pmk/arch/arm/include/armv7.h
    	modified:   air/pmk/arch/arm/include/cpu.h
    	modified:   air/pmk/arch/arm/include/svc.h
    	modified:   air/pmk/arch/arm/start.S
    	modified:   air/pmk/arch/arm/svc.c
    	modified:   air/pmk/arch/arm/virtualization_syscall.c
    
    [OC] 2018/09/28

--- air/pmk/arch/arm/svc.c
+++ air/pmk/arch/arm/svc.c
@@ -14,72 +14,175 @@
 
 #include <svc.h>
 
-air_uptr_t arm_svc_table[MAX_SVC];
+void arm_svc_handler(air_u32_t svc_id, arm_supervisor_stack_frame_t *frame,
+        pmk_core_ctrl_t *core) {
 
-air_uptr_t arm_svc_handler(arm_interrupt_stack_frame_t *frame, pmk_core_ctrl_t *core) {
+    switch (svc_id) {
+    case AIR_SYSCALL_ARM_DISABLE_INTERRUPTS:
+        arm_syscall_disable_interrupts();
+        break;
+    case AIR_SYSCALL_ARM_ENABLE_INTERRUPTS:
+        arm_syscall_enable_interrupts();
+        break;
+    case AIR_SYSCALL_ARM_DISABLE_TRAPS:
+        arm_syscall_disable_traps();
+        break;
+    case AIR_SYSCALL_ARM_ENABLE_TRAPS:
+        arm_syscall_enable_traps();
+        break;
+    case AIR_SYSCALL_ARM_DISABLE_FPU:
+        arm_syscall_disable_fpu();
+        break;
+    case AIR_SYSCALL_ARM_ENABLE_FPU:
+        arm_syscall_enable_fpu();
+        break;
+    case AIR_SYSCALL_ARM_GET_TBR:
+        frame->r0 = arm_syscall_get_tbr();
+        break;
+    case AIR_SYSCALL_ARM_GET_PSR:
+        frame->r0 = arm_syscall_get_psr();
+        break;
+    case AIR_SYSCALL_ARM_GET_IRQ_MASK_REGISTER:
+        frame->r0 = arm_syscall_get_irq_mask_register();
+        break;
 
-    air_uptr_t ret;
-    air_u32_t syscall_number = frame->reserved;
 
-    if (syscall_number < MAX_SVC) {
-        ret = ((svc) arm_svc_table[syscall_number])(frame, core);
+    case AIR_SYSCALL_ARM_SET_TBR:
+        arm_syscall_set_tbr(frame->r0);
+        break;
+    case AIR_SYSCALL_ARM_SET_PSR:
+        arm_syscall_set_psr(frame->r0);
+        break;
+    case AIR_SYSCALL_ARM_SET_IRQ_MASK_REGISTER:
+        arm_syscall_set_irq_mask_register(frame->r0);
+        break;
+
+
+    case AIR_SYSCALL_GET_P_ADDR:
+        frame->r0 = (air_u32_t)arm_get_physical_addr(core->partition->mmu_ctrl,
+                (void *)frame->r0);
+        break;
+    case AIR_SYSCALL_GET_CORE_ID:
+        frame->r0 = arm_syscall_get_core_id();
+        break;
+    //  arm_svc_install_handler(AIR_SYSCALL_BOOT_CORE, );
+    //  arm_svc_install_handler(AIR_SYSCALL_GET_US_PER_TICK, );
+    //  arm_svc_install_handler(AIR_SYSCALL_GET_ELAPSED_TICKS, );
+    case AIR_SYSCALL_GET_PARTITION_ID:
+        pmk_syscall_get_partition_id(core,
+                (air_name_ptr_t)frame->r0,
+                (air_identifier_t *)frame->r1);
+        break;
+    case AIR_SYSCALL_GET_PARTITION_STATUS:
+        frame->r0 = (air_u32_t)pmk_syscall_get_partition_status(
+                core,
+                (air_identifier_t)frame->r0,
+                (air_partition_status_t *)frame->r1);
+        break;
+    case AIR_SYSCALL_SET_PARTITION_MODE:
+        frame->r0 = (air_u32_t)pmk_syscall_set_partition_mode(
+                core,
+                (air_identifier_t)frame->r0,
+                (air_operating_mode_e)frame->r1);
+        break;
+    case AIR_SYSCALL_GET_SCHEDULE_ID:
+        frame->r0 = (air_u32_t)pmk_syscall_get_schedule_id(
+                core,
+                (air_name_ptr_t)frame->r0,
+                (air_identifier_t *)frame->r1);
+        break;
+    case AIR_SYSCALL_GET_SCHEDULE_STATUS:
+        frame->r0 = (air_u32_t)pmk_syscall_get_schedule_status(
+                core,
+                (air_schedule_status_t *)frame->r0);
+        break;
+    case AIR_SYSCALL_SET_SCHEDULE:
+        frame->r0 = (air_u32_t)pmk_syscall_set_schedule(
+                core,
+                (air_identifier_t)frame->r0);
+        break;
+    case AIR_SYSCALL_GET_TIME_OF_DAY:
+        frame->r0 = (air_u32_t)pmk_syscall_get_tod(
+                core,
+                (air_time_t *)frame->r0);
+        break;
+    case AIR_SYSCALL_SET_TIME_OF_DAY:
+        frame->r0 = (air_u32_t)pmk_syscall_set_tod(
+                core,
+                (air_time_t *)frame->r0);
+        break;
+    case AIR_SYSCALL_GET_PORT_ID:
+        frame->r0 = (air_u32_t)pmk_syscall_get_port_id(
+                core,
+                (air_port_type_e)frame->r0,
+                (air_name_ptr_t)frame->r1,
+                (air_identifier_t *)frame->r2);
+        break;
+    case AIR_SYSCALL_CREATE_PORT:
+        frame->r0 = (air_u32_t)pmk_syscall_create_port(
+                core,
+                (air_port_type_e)frame->r0,
+                (air_name_ptr_t)frame->r1,
+                (void *)frame->r2,
+                (air_identifier_t *)frame->r3);
+        break;
+    case AIR_SYSCALL_GET_PORT_STATUS:
+        frame->r0 = (air_u32_t)pmk_syscall_get_port_status(
+                core,
+                (air_port_type_e)frame->r0,
+                (air_identifier_t)frame->r1,
+                (void *)frame->r2);
+        break;
+    case AIR_SYSCALL_READ_PORT:
+        frame->r0 = (air_u32_t)pmk_syscall_read_port(
+                core,
+                (air_port_type_e)frame->r0,
+                (air_identifier_t)frame->r1,
+                (air_message_ptr_t)frame->r2,
+                (air_sz_t *)frame->r3,
+                (void *)frame->r4);
+        break;
+    case AIR_SYSCALL_WRITE_PORT:
+        frame->r0 = (air_u32_t)pmk_syscall_write_port(
+                core,
+                (air_port_type_e)frame->r0,
+                (air_identifier_t)frame->r1,
+                (air_message_ptr_t)frame->r2,
+                (air_sz_t)frame->r3,
+                (void *)frame->r4);
+        break;
+    case AIR_SYSCALL_GET_SHARED_AREA:
+        frame->r0 = (air_u32_t)pmk_syscall_get_sharedmemory(
+                core,
+                (air_name_ptr_t)frame->r0,
+                (air_sharedmemory_t *)frame->r1);
+        break;
+    //  arm_svc_install_handler(AIR_SYSCALL_SHUTDOWN_MODULE)
+    //  arm_svc_install_handler(AIR_SYSCALL_GET_EVENT)
+    //  arm_svc_install_handler(AIR_SYSCALL_GET_SYSTEM_STATE)
+    case AIR_SYSCALL_SET_SYSTEM_STATE:
+         frame->r0 = (air_u32_t)pmk_syscall_set_system_state(
+                core,
+                (air_state_e)frame->r0);
+        break;
+    case AIR_SYSCALL_GET_HM_EVENT:
+        frame->r0 = (air_u32_t)pmk_syscall_get_hm_event(
+                core,
+                (air_hm_event_t *)frame->r0);
+        break;
+    case AIR_SYSCALL_RAISE_HM_EVENT:
+        pmk_hm_isr_handler((air_error_e)frame->r0);
+        break;
+    case AIR_SYSCALL_PRINT:
+        frame->r0 = (air_u32_t)pmk_syscall_print(
+                core,
+                (char *)frame->r0,
+                (air_sz_t)frame->r1);
+        break;
+    case AIR_SYSCALL_PUTCHAR:
+        pmk_syscall_putchar(core, (char)frame->r0);
+        break;
     }
-    return ret;
-}
 
-air_uptr_t arm_svc_install_handler(air_u32_t vector, void *handler) {
-
-    air_uptr_t old_handler = (air_uptr_t)arm_svc_table[vector];
-    arm_svc_table[vector] = (air_uptr_t)handler;
-    return old_handler;
-}
-
-void arm_svc_table_initialize(void) {
-
-    arm_svc_install_handler(AIR_SYSCALL_ARM_DISABLE_INTERRUPTS, arm_syscall_disable_interrupts);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_ENABLE_INTERRUPTS, arm_syscall_enable_interrupts);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_DISABLE_INTERRUPTS, arm_syscall_disable_traps);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_ENABLE_INTERRUPTS, arm_syscall_enable_traps);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_DISABLE_FPU, arm_syscall_disable_fpu);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_ENABLE_FPU, arm_syscall_enable_fpu);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_GET_TBR, arm_syscall_get_tbr);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_SET_TBR, arm_syscall_set_tbr);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_GET_PSR, arm_syscall_get_psr);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_SET_PSR, arm_syscall_set_psr);
-//  arm_svc_install_handler(AIR_SYSCALL_ARM_RETT
-//  arm_svc_install_handler(AIR_SYSCALL_ARM_GET_CACHE_REGISTER, arm_syscall_get_cache_register);
-//  arm_svc_install_handler(AIR_SYSCALL_ARM_SET_CACHE_REGISTER, arm_syscall_set_cache_register);
-//  arm_svc_install_handler(AIR_SYSCALL_ARM_RESTORE_CACHE_REGISTER
-    arm_svc_install_handler(AIR_SYSCALL_ARM_GET_IRQ_MASK_REGISTER, arm_syscall_get_irq_mask_register);
-    arm_svc_install_handler(AIR_SYSCALL_ARM_SET_IRQ_MASK_REGISTER, arm_syscall_set_irq_mask_register);
-//  arm_svc_install_handler(AIR_SYSCALL_ARM_SET_IRQ_FORCE_REGISTER, arm_syscall_set_irq_force_register);
-
-    arm_svc_install_handler(AIR_SYSCALL_GET_P_ADDR, arm_get_physical_addr);
-//  arm_svc_install_handler(AIR_SYSCALL_GET_CORE_ID, );
-//  arm_svc_install_handler(AIR_SYSCALL_BOOT_CORE, );
-//  arm_svc_install_handler(AIR_SYSCALL_GET_US_PER_TICK, );
-//  arm_svc_install_handler(AIR_SYSCALL_GET_ELAPSED_TICKS, );
-    arm_svc_install_handler(AIR_SYSCALL_GET_PARTITION_ID, pmk_syscall_get_partition_id);
-    arm_svc_install_handler(AIR_SYSCALL_GET_PARTITION_STATUS, pmk_syscall_get_partition_status);
-    arm_svc_install_handler(AIR_SYSCALL_SET_PARTITION_MODE, pmk_syscall_set_partition_mode);
-    arm_svc_install_handler(AIR_SYSCALL_GET_SCHEDULE_ID, pmk_syscall_get_schedule_id);
-    arm_svc_install_handler(AIR_SYSCALL_GET_SCHEDULE_STATUS, pmk_syscall_get_schedule_status);
-    arm_svc_install_handler(AIR_SYSCALL_SET_SCHEDULE, pmk_syscall_set_schedule);
-    arm_svc_install_handler(AIR_SYSCALL_GET_TIME_OF_DAY, pmk_syscall_get_tod);
-    arm_svc_install_handler(AIR_SYSCALL_SET_TIME_OF_DAY, pmk_syscall_set_tod);
-    arm_svc_install_handler(AIR_SYSCALL_GET_PORT_ID, pmk_syscall_get_port_id);
-    arm_svc_install_handler(AIR_SYSCALL_CREATE_PORT, pmk_syscall_create_port);
-    arm_svc_install_handler(AIR_SYSCALL_GET_PORT_STATUS, pmk_syscall_get_port_status);
-    arm_svc_install_handler(AIR_SYSCALL_READ_PORT, pmk_syscall_read_port);
-    arm_svc_install_handler(AIR_SYSCALL_WRITE_PORT, pmk_syscall_write_port);
-    arm_svc_install_handler(AIR_SYSCALL_GET_SHARED_AREA, pmk_syscall_get_sharedmemory);
-//  arm_svc_install_handler(AIR_SYSCALL_SHUTDOWN_MODULE)
-//  arm_svc_install_handler(AIR_SYSCALL_GET_EVENT)
-//  arm_svc_install_handler(AIR_SYSCALL_GET_SYSTEM_STATE)
-    arm_svc_install_handler(AIR_SYSCALL_SET_SYSTEM_STATE, pmk_syscall_set_system_state);
-    arm_svc_install_handler(AIR_SYSCALL_GET_HM_EVENT, pmk_syscall_get_hm_event);
-    arm_svc_install_handler(AIR_SYSCALL_RAISE_HM_EVENT, pmk_hm_isr_handler);
-    arm_svc_install_handler(AIR_SYSCALL_PRINT, pmk_syscall_print);
-    arm_svc_install_handler(AIR_SYSCALL_PUTCHAR, pmk_syscall_putchar);
-
+    return;
 }
