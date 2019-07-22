32a00243c27fb709cddba10bd0faa1d0e29addfc
Luis Murta <lmurta@gmv.com>
2019-07-22 23:43:46 +0100

    Adds the partition HM handler and Lazy FPU
    Adds support for a partittion HM handler by defining the AIR_SYSCALL_ARM_RETT system call
    Exception return information duplicated at the begging of exception_hm.S to be used later when returning from exception_svc.S
    Partition handlers defined in pos/start.S and pos/exception_handler.c
    
    Lazy FPU context switch added. Undefined exception decoded to identify the FPU instruction and FPU context restore performed then. Removed from the return of the schedule IRQ
    
    HM example adds module and partition HM tables and fixes HM IDs
    
    asm_offsets and configurator updated
    
    refs #134456
    
    Changes to be committed:
    	modified:   examples/arm_unit_tests/hm/config.xml
    	modified:   examples/arm_unit_tests/hm/p0/main.c
    	modified:   libs/libair/arch/arm/syscall_paravirtualization.S
    	modified:   pmk/arch/arm/asm_offsets.c
    	modified:   pmk/arch/arm/exception_hm.S
    	modified:   pmk/arch/arm/exception_irq.S
    	modified:   pmk/arch/arm/exception_svc.S
    	new file:   pmk/arch/arm/fpu.S
    	modified:   pmk/arch/arm/hm.c
    	modified:   pmk/arch/arm/include/isr.h
    	modified:   pos/bare/arm/exception_handler.c
    	modified:   pos/bare/arm/include/bare.h
    	modified:   pos/bare/arm/init.c
    	modified:   pos/bare/arm/start.S
    	modified:   tools/configurator/air/targets/arm/zynqz1/config.py
    
    [OC] 2019/02/07

--- air/pmk/arch/arm/hm.c
+++ air/pmk/arch/arm/hm.c
@@ -21,49 +21,129 @@
 
 void arm_hm_init(void) {}
 
-void arm_hm_handler(
-        air_u32_t id, air_u32_t instr,
-        air_u32_t status, pmk_core_ctrl_t *core) {
+air_uptr_t arm_partition_hm_handler(air_u32_t id, pmk_core_ctrl_t *core) {
+
+    arm_virtual_cpu_t *vcpu = &core->context->vcpu;
+
+    air_u32_t psr = vcpu->psr;
+    air_u32_t psr_a = ((psr & ARM_PSR_A) >> 8);
+    air_u32_t **vbar = vcpu->vbar;
+
+#ifdef PMK_DEBUG_HM
+    printk("entered partition_hm_handler. vbar = 0x%x, vbar+4 = 0x%x, vpsr = 0x%x\n", (int)vbar, (int)(vbar+4), psr);
+#endif
+
+    if (vbar == NULL)
+        return NULL;
+
+    if (!psr_a) {
+
+        pmk_hm_event_t *hm_event = (pmk_hm_event_t *)core->context->hm_event;
+        if (hm_event->nesting > 0) {
+
+            vcpu->psr |= (ARM_PSR_A | ARM_PSR_I);
+
+#ifdef PMK_DEBUG_HM
+            printk("psr(A|I) = 0x%x, hm_event->error_id = %d\n", vcpu->psr, hm_event->error_id);
+#endif
+
+            switch (hm_event->error_id) {
+            case AIR_POWER_ERROR:
+                return (air_uptr_t)(vbar + 0);
+
+            case AIR_UNIMPLEMENTED_ERROR:
+                return (air_uptr_t)(vbar + 1);
+
+            case AIR_VIOLATION_ERROR:
+                return (air_uptr_t)(vbar + 3);
+
+            case AIR_SEGMENTATION_ERROR:
+                return (air_uptr_t)(vbar + 4);
+
+            case AIR_IO_ERROR:
+                // TODO not sure in this case
+                return (air_uptr_t)(vbar + 6);
+
+            case AIR_FLOAT_ERROR:
+                arm_restore_fpu(core);
+                arm_syscall_rett(core);
+                return NULL;
+
+            case AIR_ARITHMETIC_ERROR: //overflow
+            case AIR_DIVISION_BY_0_ERROR: // /0
+            default:
+                return NULL;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+air_uptr_t arm_hm_handler(air_u32_t id, air_u32_t far, air_u32_t fsr, air_u32_t *instr) {
+
+    pmk_core_ctrl_t *core = (pmk_core_ctrl_t *)arm_cp15_get_Per_CPU();
 
     air_error_e error_id;
+    air_uptr_t ret = NULL;
 
     if (id == ARM_EXCEPTION_UNDEF) {
-        /* status is the spsr */
 
 #ifdef PMK_DEBUG
-        printk("\n $$$ Undefined instr at 0x%08x : 0x%04x    spsr: 0x%08x$$$\n\n",
-                instr, (air_u16_t)*(air_u16_t *)instr, status);
+        printk("\n !!! Undefined Instruction at 0x%08x, *instr = 0x%08x, SPSR: 0x%08x !!!\n\n",
+                instr, (air_u32_t)*(air_u32_t *)instr, fsr);
 #endif
         error_id = AIR_UNIMPLEMENTED_ERROR;
+
+        /* Lazy Switching. fsr is the spsr */
+        if ((fsr && ARM_PSR_T)) {
+
+            if ( ( (*instr & 0xec00) == 0xec00 ) &&
+                    ( ((*instr & 0xf000000) == 0xa000000) || ((*instr & 0xf000000) == 0xb000000) ) ) {
+
+                error_id = AIR_FLOAT_ERROR;
+            }
+        }
 
     } else if (id == ARM_EXCEPTION_PREF_ABORT) {
 
 #ifdef PMK_DEBUG
-        printk("\n $$$ Prefetch abort at 0x%08x $$$\n\n", instr);
+        printk("\n !!! Prefetch abort at 0x%08x, *instr = 0x%04x, IFAR: 0x%08x, IFSR: 0x%08x !!!\n\n",
+                instr, (air_u16_t)*(air_u16_t *)instr, far, fsr);
 #endif
         error_id = AIR_VIOLATION_ERROR;
 
     } else if (id == ARM_EXCEPTION_DATA_ABORT) {
 
 #ifdef PMK_DEBUG
-        printk("\n $$$ Data abort at 0x%08x $$$\n\n", instr);
+        printk("\n !!! Data abort at 0x%08x, *instr = 0x%04x, DFAR: 0x%08x, DFSR: 0x%08x !!!\n\n",
+                instr, (air_u16_t)*(air_u16_t *)instr, far, fsr);
 #endif
         error_id = AIR_SEGMENTATION_ERROR;
 
     } else if (id == ARM_EXCEPTION_FIQ){
 
 #ifdef PMK_DEBUG
-        printk("\n $$$ FIIIIIIIIIIQ? $$$\n\n");
+        printk("\n !!! FIIIIIIIIIIQ? !!!\n\n");
 #endif
         error_id = AIR_IO_ERROR;
 
     } else {
 
 #ifdef PMK_DEBUG
-        printk("\n $$$ HOW? $$$\n\n");
+        printk("\n !!! HOW? !!!\n\n");
 #endif
         error_id = AIR_UNIMPLEMENTED_ERROR;
     }
 
     pmk_hm_isr_handler(error_id);
+
+    if (!core->context->trash)
+        ret = arm_partition_hm_handler(id, core);
+
+#ifdef PMK_DEBUG
+    printk("hm_handler ret = 0x%x\n", (int)ret);
+#endif
+
+    return ret;
 }
