11b46a083a35109016784f39923140e0f8539546
Luis Murta <lmurta@gmv.com>
2019-07-22 23:43:46 +0100

    Adds the partition HM handler and Lazy FPU
    Adds support for a partittion HM handler by defining the AIR_SYSCALL_ARM_RETT system call
    Exception return information duplicated at the begging of exception_hm.S to be used later when returning from exception_svc.S
    Partition handlers defined in pos/start.S and pos/exception_handler.c
    
    Lazy FPU context switch added. Undefined exception decoded to identify the FPU instruction and FPU context restore performed then. Removed from the return of the schedule IRQ
    
    HM example adds module and partition HM tables and fixes HM IDs
    
    asm_offsets and configurator updated
    
    refs #134456
    
    Changes to be committed:
    	modified:   examples/arm_unit_tests/hm/config.xml
    	modified:   examples/arm_unit_tests/hm/p0/main.c
    	modified:   libs/libair/arch/arm/syscall_paravirtualization.S
    	modified:   pmk/arch/arm/asm_offsets.c
    	modified:   pmk/arch/arm/exception_hm.S
    	modified:   pmk/arch/arm/exception_irq.S
    	modified:   pmk/arch/arm/exception_svc.S
    	new file:   pmk/arch/arm/fpu.S
    	modified:   pmk/arch/arm/hm.c
    	modified:   pmk/arch/arm/include/isr.h
    	modified:   pos/bare/arm/exception_handler.c
    	modified:   pos/bare/arm/include/bare.h
    	modified:   pos/bare/arm/init.c
    	modified:   pos/bare/arm/start.S
    	modified:   tools/configurator/air/targets/arm/zynqz1/config.py
    
    [OC] 2019/02/07

--- air/pmk/arch/arm/include/isr.h
+++ air/pmk/arch/arm/include/isr.h
@@ -22,9 +22,7 @@
 #include <workspace.h>
 
 void arm_hm_init(void);
-void arm_hm_handler(
-        air_u32_t id, air_u32_t instr,
-        air_u32_t status, pmk_core_ctrl_t *core);
+air_uptr_t arm_hm_handler(air_u32_t id, air_u32_t fsr, air_u32_t far, air_u32_t *instr);
 air_uptr_t arm_partition_hm_handler(air_u32_t id, pmk_core_ctrl_t *core);
 
 void arm_isr_table_init(void);
@@ -36,5 +34,7 @@ void arm_isr_default_handler(arm_interrupt_stack_frame_t *frame,
         pmk_core_ctrl_t *core);
 
 air_uptr_t arm_isr_install_handler(air_u32_t vector, void *handler);
+
+void arm_restore_fpu(pmk_core_ctrl_t *core);
 
 #endif /* ARM_ISR_H */
