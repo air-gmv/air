228cfeeb5458140808ada761423b398422d424ef
Daniel Silveira <dtms@gmv.com>
2020-02-20 18:41:39 +0000

    Set C source code to year 2020

--- air/pmk/core/init.c
+++ air/pmk/core/init.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2019  GMVIS Skysoft S.A.
+ * Copyright (C) 2015-2020  GMVIS Skysoft S.A.
  *
  * The license and distribution terms for this file may be
  * found in the file LICENSE in this distribution or at
