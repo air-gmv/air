b802a9b109cc7325783c69d9db086e961f4e4f0d
Luis Murta <lmurta@gmv.com>
2020-03-23 13:07:27 +0000

    Fixes wrong CAN driver pointer on xparameters.h
    The xparameters.h file will change based on the board configuration
    This file will need to be moved to the examples folder

--- air/libs/libiop/drivers/xilinx_hal/include/xparameters.h
+++ air/libs/libiop/drivers/xilinx_hal/include/xparameters.h
@@ -34,20 +34,20 @@
 /* Definitions for driver CANPS */
 #define XPAR_XCANPS_NUM_INSTANCES 1
 
-/* Definitions for peripheral PS7_CAN_1 */
-#define XPAR_PS7_CAN_1_DEVICE_ID 0
-#define XPAR_PS7_CAN_1_BASEADDR 0xE0009000
-#define XPAR_PS7_CAN_1_HIGHADDR 0xE0009FFF
-#define XPAR_PS7_CAN_1_CAN_CLK_FREQ_HZ 40000000
+/* Definitions for peripheral PS7_CAN_0 */
+#define XPAR_PS7_CAN_0_DEVICE_ID 0
+#define XPAR_PS7_CAN_0_BASEADDR 0xE0008000
+#define XPAR_PS7_CAN_0_HIGHADDR 0xE0008FFF
+#define XPAR_PS7_CAN_0_CAN_CLK_FREQ_HZ 23809525
 
 
 /******************************************************************/
 
-/* Canonical definitions for peripheral PS7_CAN_1 */
-#define XPAR_XCANPS_0_DEVICE_ID XPAR_PS7_CAN_1_DEVICE_ID
-#define XPAR_XCANPS_0_BASEADDR 0xE0009000
-#define XPAR_XCANPS_0_HIGHADDR 0xE0009FFF
-#define XPAR_XCANPS_0_CAN_CLK_FREQ_HZ 40000000
+/* Canonical definitions for peripheral PS7_CAN_0 */
+#define XPAR_XCANPS_0_DEVICE_ID XPAR_PS7_CAN_0_DEVICE_ID
+#define XPAR_XCANPS_0_BASEADDR 0xE0008000
+#define XPAR_XCANPS_0_HIGHADDR 0xE0008FFF
+#define XPAR_XCANPS_0_CAN_CLK_FREQ_HZ 23809525
 
 
 /******************************************************************/
