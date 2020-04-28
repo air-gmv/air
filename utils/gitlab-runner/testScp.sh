4a0db6104b92fa4f838a76f7ecf91d2255d52ac5
Daniel Silveira <dtms@gmv.es>
2020-04-28 10:53:33 +0000

    The Issue #49 is solved with the following changes:
    - Test generator creates always 2 MTF in configuration
    8973d1c6 - Set all MTF with default 0.3 and 1,5 seconds
    cf0fcb0a - Upgrade of TEST-DEF-0009 with new Schedule
    08e07149 - Upgrading all tests with latest testgen
    62474de9 - First version of ARM QEMU Test running in gitlab runner
    9fcd39a2 - Update .gitlab-ci.yml
    8c4bfe40 - Created testcheck.py and testresults.txt output
    1136be97 - Set telnet as paralell process, Add telnet.sh
    5c6a1dee - Full test to close issue
    
    Refs #49

--- utils/gitlab-runner/testScp.sh
+++ utils/gitlab-runner/testScp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd executable/
-sshpass -p '$(AIR_RUNNER_PASSWORD)' scp AIRAPP.exe $(AIR_RUNNER_USER)@$(AIR_RUNNER_IP):~
+sshpass -p $PASSWORD1 scp AIRAPP.exe $(AIR_RUNNER_USER)@$(AIR_RUNNER_IP):~
 cd ..
-sshpass -p '$(AIR_RUNNER_PASSWORD)' scp runAIRAPP.sh $(AIR_RUNNER_USER)@$(AIR_RUNNER_IP):~
-sshpass -p '$(AIR_RUNNER_PASSWORD)' ssh $(AIR_RUNNER_USER)@$(AIR_RUNNER_IP) 'ls'
+sshpass -p $PASSWORD1 scp runAIRAPP.sh $(AIR_RUNNER_USER)@$(AIR_RUNNER_IP):~
+sshpass -p $PASSWORD1 ssh $(AIR_RUNNER_USER)@$(AIR_RUNNER_IP) 'ls'
 
