475a03c4a18091080c741061e00610964a49c9ef
Daniel Silveira <dtms@gmv.es>
2020-04-29 17:27:54 +0000

    Update do_zynqz1_qemu.sh
    Refs #46

--- utils/gitlab-runner/do_zynqz1_qemu.sh
+++ utils/gitlab-runner/do_zynqz1_qemu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 killall qemu-system-arm
 killall tee
-./telnet.sh &
+./killqemu.sh &
 /home/taste/work/qemu/arm-softmmu/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/taste/work/qemu/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt
 
 
