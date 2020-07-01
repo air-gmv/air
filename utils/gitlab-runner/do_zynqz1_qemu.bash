875114fcc9025bfe866b410fad705cdac2f374f3
Daniel Silveira <dtms@gmv.es>
2020-07-01 11:32:34 +0000

    Resolve "RTEMS RSB for ARM require GLIBC 2.27" Add new debian VM supporting GLIBV 2.27 to use in CI/CD.

--- utils/gitlab-runner/do_zynqz1_qemu.bash
+++ utils/gitlab-runner/do_zynqz1_qemu.bash
@@ -2,7 +2,8 @@
 configure
 make clean && make
 ./killqemu.bash &
-/home/taste/work/qemu/arm-softmmu/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/taste/work/qemu/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt cat testresult.txt
+/opt/qemu/bin/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
+cat testresult.txt
 
 
 
