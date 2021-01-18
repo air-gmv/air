b301b9ec48f44044c8741092085e41bc3b18dad8
Carolina Pinto dos Santos Serra <cpserra@gmv.com>
2021-01-18 18:18:45 +0000

    ARM return from exception
    In a normal return from exception, which does not redirect to a virtual exception handler in the POS, the SUBS instruction was replaced by a RFE.
    In the case of virtual interrupts, this replacement is unnecessary and would require more changes to the code, so the SUBS instruction was kept.

--- utils/gitlab-runner/do_zynqz1_qemu.bash
+++ utils/gitlab-runner/do_zynqz1_qemu.bash
@@ -5,4 +5,13 @@ make clean && make
 /opt/qemu/bin/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
 
 
-
+cmd="./testcheck.py"
+$cmd
+status=$?
+if [ $status -eq 0 ]; then
+    echo TEST RTEMS5 OK
+else
+    echo TEST RTEMS5 FAILED EXITING
+    exit 1
+fi
+echo RTEMS5 test ended
