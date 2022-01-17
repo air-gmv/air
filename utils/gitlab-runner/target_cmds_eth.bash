98ed0f369d1a4b5cc8c16d6e3c890405a806f828
Joao Andrade <joao.andrade@colabatlantic.com>
2022-01-17 17:12:55 +0100

    Switched to nohup to see if test runs

--- utils/gitlab-runner/target_cmds_eth.bash
+++ utils/gitlab-runner/target_cmds_eth.bash
@@ -3,6 +3,7 @@ wall << EFO
 The Automatic Test of gitlab will try run test on board N2X
 EFO
 
-./doTest.bash
-# nohup ./doTest.bash &
-# ./host_test.py
+nohup ./doTest.bash &
+./host_test.py
+
+cat nohup.out
