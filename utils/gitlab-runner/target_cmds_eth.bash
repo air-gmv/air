1a0ca6b741c2e4117fc382ad9304947776ade902
Joao Andrade <joao.andrade@colabatlantic.com>
2022-01-18 10:54:31 +0100

    Changed target_cmds_eth.bash to include all board commands.

--- utils/gitlab-runner/target_cmds_eth.bash
+++ utils/gitlab-runner/target_cmds_eth.bash
@@ -3,7 +3,15 @@ wall << EFO
 The Automatic Test of gitlab will try run test on board N2X
 EFO
 
-./doTest.bash &
+ps -uax | grep grmon
+lockfile -s 5 -r 5 /var/lock/gr740.grmonlock || exit 1
+rm -f /var/lock/gr740.grmonlock
+./killgrmon.bash &
+board-commands.sh runAIRAPP.bash &
 ./host_test.py &
 wait
+ls
+killall sleep
+killall -9 grmon
+killall killgrmon.bash
 exit
