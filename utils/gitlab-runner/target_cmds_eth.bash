e1161d68b86e665c118ebbec44d595f8ee53c5c0
Joao Andrade <joao.andrade@colabatlantic.com>
2022-01-18 16:28:11 +0100

    Revert "Removed nohup and switched to background running + wait command"
    This reverts commit 9978b99c5136c51580919638fbd30466c1f71d15.

--- utils/gitlab-runner/target_cmds_eth.bash
+++ utils/gitlab-runner/target_cmds_eth.bash
@@ -3,6 +3,7 @@ wall << EFO
 The Automatic Test of gitlab will try run test on board N2X
 EFO
 
+<<<<<<< HEAD
 ps -uax | grep grmon
 lockfile -s 5 -r 5 /var/lock/gr740.grmonlock || exit 1
 rm -f /var/lock/gr740.grmonlock
@@ -15,3 +16,9 @@ killall sleep
 killall -9 grmon
 killall killgrmon.bash
 exit
+=======
+nohup ./doTest.bash &
+./host_test.py
+
+cat nohup.out
+>>>>>>> parent of bef52188... Removed nohup and switched to background running + wait command
