d088e136ea5e32fc3382bd4f2be02a89e848d68e
Joao Andrade <joao.andrade@colabatlantic.com>
2022-01-18 18:00:36 +0100

    Added applicable doTest.bash commands to target_cmds_eth.bash

--- utils/gitlab-runner/target_cmds_eth.bash
+++ utils/gitlab-runner/target_cmds_eth.bash
@@ -3,7 +3,13 @@ wall << EFO
 The Automatic Test of gitlab will try run test on board N2X
 EFO
 
-nohup ./doTest.bash &
+ps -uax | grep grmon
+killall -9 grmon
+nohup board-commands.sh runAIRAPP.bash &
 ./host_test.py
 
 cat nohup.out
+
+ls
+killall -9 grmon
+exit
