d154a8dd28814bb03cab82181f384ac4c8ccc62d
João Andrade <exjmda@gmv.com>
2022-09-16 15:32:48 +0000

    Configure gitlab-ci.yaml to use laysim
    Add support for multiple job processing using rsync
    
    Add ports example to coverage report
    
    Add additional tags and miscellaneous fixes
    
    Add other builds

---
+++ utils/gitlab-runner/publish_to_central_server.bash
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PC=$(uname -n)
+
+
+function write() {
+	
+	if test $PC != "$1" ; then
+			rsync -azvrh $STATE_FOLDER gitlab-runner@$2:~/state/
+			echo "Published files to main server."
+	fi
+
+}
+
+if ! test -e /tmp/rsync-write-"$CI_COMMIT_SHORT_SHA".lock ; then
+	touch /tmp/rsync-write-"$CI_COMMIT_SHORT_SHA".lock
+else 
+	echo "Waiting for another job to finish writing to main server..."
+	while test -e /tmp/rsync-write-"$CI_COMMIT_SHORT_SHA".lock
+	do
+		sleep 1
+	done
+	touch /tmp/rsync-write-"$CI_COMMIT_SHORT_SHA".lock
+	echo "Writing to main server..."
+fi
+
+case $CI_RUNNER_TAGS in
+	*SPARC* )
+		write "TASTE-VM-9-1-beta2-64bit-1" $(AIR_SPARC_1_IP)
+		;;
+	*ARM* )
+		write "AIR-ARM-VM-debian-64-1" $(AIR_ARM_1_IP)
+		;;
+	*OTHER* )
+		write "TASTE-VM-9-1-beta2-64bit-2" $(AIR_SPARC_2_IP)
+		;;
+	*)
+		echo "Invalid hostname $PC"
+		exit 1
+		;;
+esac
+
+rm /tmp/rsync-write-"$CI_COMMIT_SHORT_SHA".lock
+
+exit 0
