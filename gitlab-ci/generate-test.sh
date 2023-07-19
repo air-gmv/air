b1c7204b2f95811ccd32396eabcd7735a9e68c8c
trdjlf <dferreira@gmv.com>
2023-07-19 14:06:39 +0200

    [SPARC] Add ARM temp to generate-test (00510)

--- gitlab-ci/generate-test.sh
+++ gitlab-ci/generate-test.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 # Access the variables passed as command-line arguments
-TEST_NUMBER=$1
-AIR=$2
+AIR=$1
+UTILS=$2
+ARCH=$3
+TEST_NUMBER=$4
 
 # Generate the YAML content
 cat > test.yml <<EOF
+.ARM-QEMU-validation-template:
+    stage: validation_tests
+    needs: ["ARM-QEMU-hello_world","ARM-QEMU-bare_c"]
+    tags:  ["ARM"]
+    script: 
+        - cd $AIR/examples/private-example/private/validation/TEST-DEF-$TEST_NUMBER
+        - *build-executable
+        - ($UTILS/rvs_arm_coverage-ci.bash | tee testresult.txt) || true
+        - *check-and-publish
+    rules:
+        - if: "$ARCH == "ARM""
 
 .SPARC-LAYSIM-validation-template:
   stage: validation_tests
@@ -18,7 +31,12 @@ cat > test.yml <<EOF
         - laysim-gr740-mmu-cli -batch ../../../../../rvs_air/scripts/laysim_cmds.txt | tee testresult.txt
         - make distclean -i # Ignore distclean errors
         - *check-and-publish
-        
+  rules:
+        - if: "$ARCH == "SPARC""
+
+ARM-QEMU-TEST-DEF-$TEST_NUMBER:
+    extends: .ARM-QEMU-validation-template  
+
 SPARC-LAYSIM-TEST-DEF-$TEST_NUMBER:
     extends: .SPARC-LAYSIM-validation-template
 
