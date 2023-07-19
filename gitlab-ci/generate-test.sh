5e92f66161f71ce6d4ed7e8f6d38edbc067c2caa
trdjlf <dferreira@gmv.com>
2023-07-19 18:44:10 +0200

    [SPARC] Add beforescript to child pipeline (00510)

--- gitlab-ci/generate-test.sh
+++ gitlab-ci/generate-test.sh
@@ -20,6 +20,58 @@ cat > test.yml <<EOF
 
 .ARM-QEMU-validation-template:
   tags:  ["ARM"]
+  before_script:
+        - sleep \$((\$RANDOM % 2)).\$RANDOM
+
+        - export STATE_FOLDER=\$HOME/state/\$CI_COMMIT_SHA
+        - export GIT_SSL_NO_VERIFY=1
+
+        - git config --global credential.helper store
+        - echo "https://huca:\${CI_JOB_TOKEN}@$(AIR_GIT_REMOTE_HOSTNAME)" > ~/.git-credentials
+
+        - if ! test -f git-clean.lock; 
+            then 
+                touch git-clean.lock; 
+            fi
+
+        - if ! \$(grep -Fxq \$CI_COMMIT_SHORT_SHA git-clean.lock); 
+            then 
+                git clean -ffdx && git submodule foreach --recursive git clean -ffdx && echo \$CI_COMMIT_SHORT_SHA > git-clean.lock; 
+                git submodule update --init --recursive; 
+                git submodule sync --recursive; 
+            fi
+            
+        - if ! test -d \$STATE_FOLDER; 
+            then 
+                (mkdir -p \$STATE_FOLDER || true) && ./utils/gitlab-runner/check_central_server.bash; 
+            else 
+                ./utils/gitlab-runner/check_central_server.bash;
+            fi;
+
+        - while test -e /tmp/rsync-"\$CI_COMMIT_SHORT_SHA".lock; 
+            do sleep 1; 
+            done;        
+
+        - cd \$STATE_FOLDER
+        - cd air
+        
+        - export AIR=\$STATE_FOLDER/air/
+        - export PATH=\$PATH:\$AIR
+        - export AIR_INSTALL=\$AIR/install
+        - export AIR_PMK=\$AIR_INSTALL/pmk
+        - export AIR_POS=\$AIR_INSTALL/pos
+
+        - export RTEMS410=/opt/rtems-4.10/bin
+        - export PATH=\$PATH:\$RTEMS410
+        - export RTEMS_MAKEFILE_PATH=\$AIR_POS/rtems5/rtems5-install/sparc-rtems5/gr740
+        - export PATH=\$PATH:/opt/gcc-arm-9.2-2019.12-x86_64-arm-none-eabi/bin
+
+        - export DISPLAY=:99
+        - export PATH=/opt/rtems/5/bin:\$PATH
+        - export PATH=/opt/rtems-5.1-2019.07.25/bin:\$PATH
+        - export PATH=/opt/laysim-gr740:\$PATH
+
+        - export UTILS=\$AIR/../utils/gitlab-runner
   script: 
       - cd $AIR/examples/private-example/private/validation/TEST-DEF-$TEST_NUMBER
       - *build-executable
@@ -30,6 +82,58 @@ cat > test.yml <<EOF
 
 .SPARC-LAYSIM-validation-template:
   tags:  ["SPARC","LAYSIM"]
+  before_script:
+        - sleep \$((\$RANDOM % 2)).\$RANDOM
+
+        - export STATE_FOLDER=\$HOME/state/\$CI_COMMIT_SHA
+        - export GIT_SSL_NO_VERIFY=1
+
+        - git config --global credential.helper store
+        - echo "https://huca:\${CI_JOB_TOKEN}@$(AIR_GIT_REMOTE_HOSTNAME)" > ~/.git-credentials
+
+        - if ! test -f git-clean.lock; 
+            then 
+                touch git-clean.lock; 
+            fi
+
+        - if ! \$(grep -Fxq \$CI_COMMIT_SHORT_SHA git-clean.lock); 
+            then 
+                git clean -ffdx && git submodule foreach --recursive git clean -ffdx && echo \$CI_COMMIT_SHORT_SHA > git-clean.lock; 
+                git submodule update --init --recursive; 
+                git submodule sync --recursive; 
+            fi
+            
+        - if ! test -d \$STATE_FOLDER; 
+            then 
+                (mkdir -p \$STATE_FOLDER || true) && ./utils/gitlab-runner/check_central_server.bash; 
+            else 
+                ./utils/gitlab-runner/check_central_server.bash;
+            fi;
+
+        - while test -e /tmp/rsync-"\$CI_COMMIT_SHORT_SHA".lock; 
+            do sleep 1; 
+            done;        
+
+        - cd \$STATE_FOLDER
+        - cd air
+        
+        - export AIR=\$STATE_FOLDER/air/
+        - export PATH=\$PATH:\$AIR
+        - export AIR_INSTALL=\$AIR/install
+        - export AIR_PMK=\$AIR_INSTALL/pmk
+        - export AIR_POS=\$AIR_INSTALL/pos
+
+        - export RTEMS410=/opt/rtems-4.10/bin
+        - export PATH=\$PATH:\$RTEMS410
+        - export RTEMS_MAKEFILE_PATH=\$AIR_POS/rtems5/rtems5-install/sparc-rtems5/gr740
+        - export PATH=\$PATH:/opt/gcc-arm-9.2-2019.12-x86_64-arm-none-eabi/bin
+
+        - export DISPLAY=:99
+        - export PATH=/opt/rtems/5/bin:\$PATH
+        - export PATH=/opt/rtems-5.1-2019.07.25/bin:\$PATH
+        - export PATH=/opt/laysim-gr740:\$PATH
+
+        - export UTILS=\$AIR/../utils/gitlab-runner
   script: 
         - echo "The value of TEST_NUMBER is $TEST_NUMBER"
         - cd $AIR/examples/private-example/private/validation/TEST-DEF-$TEST_NUMBER
