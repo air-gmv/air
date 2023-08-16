1f7e3d3894ac243e16e7357f741dd80c8dabb6de
trdjlf <dferreira@gmv.com>
2023-08-16 16:23:42 +0200

    [SPARC] Copy executable to home directory

--- utils/gitlab-runner/gdb_sparc_rvs_coverage-ci.gdb
+++ utils/gitlab-runner/gdb_sparc_rvs_coverage-ci.gdb
@@ -3,6 +3,7 @@ set pagination off
 set confirm off
 set print elements 0
 
+target remote localhost:1234
 tbreak RVS_Output
 continue 
 dump memory memdump.bin &rvs_all_data32_array &rvs_all_data32_array+1
--- utils/gitlab-runner/target_cmds_eth_sparc.bash
+++ utils/gitlab-runner/target_cmds_eth_sparc.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 wall << EFO
-The Automatic Test of gitlab will try run test on board N2X
+The Automatic Test of gitlab will try run test on board GR740
 EFO
 
 ps -uax | grep grmon
@@ -12,7 +12,7 @@ killall -9 grmon
 
 /opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb -c runAIRAPP.bash -echo &
 
-/opt/rtems5/bin//sparc-rtems5-gdb -x gdb_sparc_rvs_coverage-ci /home/gitlabrunner/AIRAPP.exe --cd=.
+/opt/rtems5/bin//sparc-rtems5-gdb -x gdb_sparc_rvs_coverage-ci.gdb AIRAPP.exe --cd=.
 
 ls
 killall -9 grmon
