d7cb623729c3c643329fd6968afbdcf6346ab164
trdjlf <dferreira@gmv.com>
2023-08-21 18:42:26 +0200

    [SPARC] Fix typo

--- utils/gitlab-runner/target_cmds_eth_sparc.bash
+++ utils/gitlab-runner/target_cmds_eth_sparc.bash
@@ -7,7 +7,7 @@ ps -uax | grep grmon
 killall -9 grmon
 
 /opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb
-/opt/rtems5/bin//sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage_ci.gdb
+/opt/rtems5/bin//sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci.gdb
 
 killall -9 grmon
 
