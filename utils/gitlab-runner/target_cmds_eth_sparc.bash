0ea68a26afbe8bb9d9abc933756e7eea5744e360
trdjlf <dferreira@gmv.com>
2023-08-21 19:18:28 +0200

    [SPARC] Add nohup to grmon command

--- utils/gitlab-runner/target_cmds_eth_sparc.bash
+++ utils/gitlab-runner/target_cmds_eth_sparc.bash
@@ -6,9 +6,11 @@ EFO
 ps -uax | grep grmon
 killall -9 grmon
 
-/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb
+#/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb
+nohup /opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb &
+
 #/opt/rtems5/bin//sparc-rtems5-gdb -v AIRAPP.exe -x gdb_sparc_rvs_coverage-ci.gdb
-xterm -e "/opt/rtems5/bin/sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci.gdb > output.txt"
+/opt/rtems5/bin/sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci.gdb
 
 cat output.txt
 
