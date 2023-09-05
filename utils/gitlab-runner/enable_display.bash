1c76bdf60e9911871b56765cbd24c85544fe78e7
trdjlf <dferreira@gmv.com>
2023-09-05 17:54:12 +0200

    [ALL] Trigger merge pipeline

--- utils/gitlab-runner/enable_display.bash
+++ utils/gitlab-runner/enable_display.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+TASTE_PW=$1
+
 # Check if the Xvfb process is running
 if ps aux | grep "[X]vfb" >/dev/null; then
     echo "The Xvfb process is running. Continuing."
 else
     echo "The Xvfb process is not running. Starting it now..."
-    sudo Xvfb :99 &
+    echo "$TASTE_PW" | sudo -S Xvfb :99 &
     echo "The Xvfb process was successfully started."
 fi
