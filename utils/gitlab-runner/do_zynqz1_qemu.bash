73b899f17e5d0d74a4a71171523ec3acaf85b789
trdjlf <dferreira@gmv.com>
2023-09-06 15:16:36 +0200

    [ARM] Change testcheck script

--- utils/gitlab-runner/do_zynqz1_qemu.bash
+++ utils/gitlab-runner/do_zynqz1_qemu.bash
@@ -2,10 +2,10 @@
 UTILS=$1
 #configure
 #make clean & make
-./killqemu.bash &
+. $UTILS/killqemu.bash &
 /opt/qemu/bin/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
 
-cmd="./$UTILS/testcheck_arm.py"
+cmd=". $UTILS/testcheck_arm.py"
 $cmd
 status=$?
 if [ $status -eq 0 ]; then
