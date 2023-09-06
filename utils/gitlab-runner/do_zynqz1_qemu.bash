ea8d3c1001521cf525e788dc4e3524c1c6cad213
trdjlf <dferreira@gmv.com>
2023-09-06 16:09:17 +0200

    [ARM] Remove pkill from if statement

--- utils/gitlab-runner/do_zynqz1_qemu.bash
+++ utils/gitlab-runner/do_zynqz1_qemu.bash
@@ -5,7 +5,7 @@ UTILS=$1
 pkill qemu-system-arm
 /opt/qemu/bin/qemu-system-arm -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
 wait
-sleep 20
+sleep 60
 
 cmd=". $UTILS/testcheck_arm.py"
 $cmd
@@ -14,7 +14,6 @@ if [ $status -eq 0 ]; then
     echo TEST RTEMS5 OK
 else
     echo TEST RTEMS5 FAILED EXITING
-    pkill qemu-system-arm
     exit 1
 fi
 echo RTEMS5 test ended
