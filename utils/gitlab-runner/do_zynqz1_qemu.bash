4e4870ed0b61692607e6c5fc6a95c988d07d36bd
trdjlf <dferreira@gmv.com>
2023-09-06 14:46:07 +0200

    [ARM] Trigger pipeline

--- utils/gitlab-runner/do_zynqz1_qemu.bash
+++ utils/gitlab-runner/do_zynqz1_qemu.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
-configure
-make clean && make
+UTILS=$1
+#configure
+#make clean & make
 ./killqemu.bash &
 /opt/qemu/bin/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
 
-
-cmd="./testcheck_arm.py"
+cmd="./$UTILS/testcheck_arm.py"
 $cmd
 status=$?
 if [ $status -eq 0 ]; then
