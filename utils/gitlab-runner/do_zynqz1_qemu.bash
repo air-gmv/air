04291489acf6c7eaaaef535fb15909bb134676f9
trdjlf <dferreira@gmv.com>
2023-09-06 15:27:03 +0200

    [ARM] Change pkill command

--- utils/gitlab-runner/do_zynqz1_qemu.bash
+++ utils/gitlab-runner/do_zynqz1_qemu.bash
@@ -2,7 +2,7 @@
 UTILS=$1
 #configure
 #make clean & make
-. $UTILS/killqemu.bash &
+pkill qemu-system-arm
 /opt/qemu/bin/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
 
 cmd=". $UTILS/testcheck_arm.py"
@@ -12,6 +12,10 @@ if [ $status -eq 0 ]; then
     echo TEST RTEMS5 OK
 else
     echo TEST RTEMS5 FAILED EXITING
+    pkill qemu-system-arm
     exit 1
 fi
 echo RTEMS5 test ended
+pkill qemu-system-arm
+
+
