75e4e842875bce02cd3a2369b895bc47a0f251dd
trdjlf <dferreira@gmv.com>
2023-09-06 18:28:18 +0200

    [ARM] Add monitor tag

--- utils/gitlab-runner/do_zynqz1_qemu.bash
+++ utils/gitlab-runner/do_zynqz1_qemu.bash
@@ -3,7 +3,7 @@ UTILS=$1
 #configure
 #make clean & make
 pkill qemu-system-arm
-/opt/qemu/bin/qemu-system-arm -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
+/opt/qemu/bin/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
 #wait
 #sleep 60
 
