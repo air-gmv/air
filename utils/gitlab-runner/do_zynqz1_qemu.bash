7e3740b576570a27ee2f5b7cc375fb1232e117d0
trdjlf <dferreira@gmv.com>
2023-09-06 16:02:16 +0200

    [ARM] Add sleep to script

--- utils/gitlab-runner/do_zynqz1_qemu.bash
+++ utils/gitlab-runner/do_zynqz1_qemu.bash
@@ -3,8 +3,9 @@ UTILS=$1
 #configure
 #make clean & make
 pkill qemu-system-arm
-/opt/qemu/bin/qemu-system-arm -monitor telnet::45454,server,nowait -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
+/opt/qemu/bin/qemu-system-arm -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -kernel executable/AIRAPP.exe |& tee testresult.txt 
 wait
+sleep 20
 
 cmd=". $UTILS/testcheck_arm.py"
 $cmd
@@ -17,6 +18,6 @@ else
     exit 1
 fi
 echo RTEMS5 test ended
-pkill qemu-system-arm
+
 
 
