2b2f4661ad9af137f87b83fbe4ad120f9cdc13d5
trdjlf <dferreira@gmv.com>
2023-09-08 15:32:28 +0200

    Copy gdb output to runner

--- utils/gitlab-runner/target_cmds_eth_sparc.bash
+++ utils/gitlab-runner/target_cmds_eth_sparc.bash
@@ -9,10 +9,8 @@ pkill -f "/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb"
 #/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb
 nohup /opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb &
 
-#/opt/rtems5/bin//sparc-rtems5-gdb -v AIRAPP.exe -x gdb_sparc_rvs_coverage-ci.gdb
+#/opt/rtems5/bin//sparc-rtems5-gdb -v AIRAPP.exe -x gdb_sparc_rvs_coverage-ci
 /opt/rtems5/bin/sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci > gdb_output.txt 2>&1
-
-cat gdb_output.txt
 
 pkill -f "/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb"
 
