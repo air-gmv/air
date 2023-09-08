04eca070e7b5a7fd82b276d7c32d708cc97b1637
trdjlf <dferreira@gmv.com>
2023-09-08 15:23:34 +0200

    Remove .gdb

--- utils/gitlab-runner/target_cmds_eth_sparc.bash
+++ utils/gitlab-runner/target_cmds_eth_sparc.bash
@@ -10,7 +10,7 @@ pkill -f "/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb"
 nohup /opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb &
 
 #/opt/rtems5/bin//sparc-rtems5-gdb -v AIRAPP.exe -x gdb_sparc_rvs_coverage-ci.gdb
-/opt/rtems5/bin/sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci.gdb > gdb_output.txt 2>&1
+/opt/rtems5/bin/sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci > gdb_output.txt 2>&1
 
 cat gdb_output.txt
 
