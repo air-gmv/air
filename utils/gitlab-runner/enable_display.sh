c840dc2966a38787aec9247e44bbfe4a5fbdd1b3
trdjlf <dferreira@gmv.com>
2023-09-11 15:49:39 +0200

    [SPARC] Remove background flag

--- utils/gitlab-runner/enable_display.sh
+++ utils/gitlab-runner/enable_display.sh
@@ -7,8 +7,9 @@ if ps aux | grep "[X]vfb" >/dev/null; then
     echo "The Xvfb process is running. Continuing."
 else
     echo "The Xvfb process is not running. Starting it now..."
-    sudo -b -n -S <<< "$TASTE_PW" Xvfb :99 &
+    sudo -b -n -S <<< "$TASTE_PW" Xvfb :99
     echo "The Xvfb process was successfully started."
+    # currently gets stuck on the pipeline past this point
 fi
 
 exit 0
