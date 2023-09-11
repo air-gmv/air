8d9b3ac3c21fd5d4d64ba7218093b4caf33f808e
trdjlf <dferreira@gmv.com>
2023-09-11 15:39:13 +0200

    [SPARC] Add -b and -n flags to displays script

--- utils/gitlab-runner/enable_display.sh
+++ utils/gitlab-runner/enable_display.sh
@@ -7,7 +7,7 @@ if ps aux | grep "[X]vfb" >/dev/null; then
     echo "The Xvfb process is running. Continuing."
 else
     echo "The Xvfb process is not running. Starting it now..."
-    sudo -S <<< "$TASTE_PW" Xvfb :99 &
+    sudo -b -n -S <<< "$TASTE_PW" Xvfb :99 &
     echo "The Xvfb process was successfully started."
 fi
 
