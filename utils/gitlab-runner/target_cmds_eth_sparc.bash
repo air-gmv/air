22c47ab1532f4f8f0e2b8d5736c9e13cd01ef804
trdjlf <dferreira@gmv.com>
2023-09-12 18:48:33 +0200

    Add sleep to SPARC scheduled script

--- utils/gitlab-runner/target_cmds_eth_sparc.bash
+++ utils/gitlab-runner/target_cmds_eth_sparc.bash
@@ -10,7 +10,7 @@ pkill -f "/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb"
 
 nohup /opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb &
 
-sleep 5
+sleep 10
 
 /opt/rtems5/bin/sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci > gdb_output.txt 2>&1
 
