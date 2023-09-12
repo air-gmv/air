6e6d9bdacba9021208bb84afab9d1fa59d8d1095
trdjlf <dferreira@gmv.com>
2023-09-12 12:38:28 +0200

    Fix syntax error in script output

--- utils/gitlab-runner/target_cmds_eth_sparc.bash
+++ utils/gitlab-runner/target_cmds_eth_sparc.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-$EXAMPLE=$1
+EXAMPLE=$1
 wall << EFO
 GitLab CI/CD: SPARC test $EXAMPLE will try to run on board GR740
 EFO
 
-ps -uax | grep grmon
-pkill -f "/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb"
-
-nohup /opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb &
-/opt/rtems5/bin/sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci > gdb_output.txt 2>&1
-
-pkill -f "/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb"
-
+#ps -uax | grep grmon
+#pkill -f "/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb"
+#
+#nohup /opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb &
+#/opt/rtems5/bin/sparc-rtems5-gdb AIRAPP.exe -x gdb_sparc_rvs_coverage-ci > gdb_output.txt 2>&1
+#
+#pkill -f "/opt/grmon-pro-2.0.92/bin/grmon -u -nb -eth 192.168.0.30 -gdb"
+#
 wall << EFO
 GitLab CI/CD: SPARC test $EXAMPLE has finished executing
 EFO
