f41757116c09ddf8b52f5d1bb20fddbeff1c6cd6
trdjlf <dferreira@gmv.com>
2023-09-13 16:17:33 +0200

    Change ARM pkill and kill by PID

--- utils/gitlab-runner/rvs_arm_coverage-ci.bash
+++ utils/gitlab-runner/rvs_arm_coverage-ci.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+pkill -f "/opt/qemu/bin/qemu-system-arm -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -s -kernel ./executable/AIRAPP.exe -S"
 pkill qemu-system-arm
-/opt/qemu/bin/qemu-system-arm -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -s -kernel ./executable/AIRAPP.exe -S &
 
-arm-rtems5-gdb -x $AIR/../utils/gitlab-runner/gdb_arm_rvs_coverage-ci ./executable/AIRAPP.exe --cd=.
-pkill qemu-system-arm
+/opt/qemu/bin/qemu-system-arm -M arm-generic-fdt-7series -serial mon:stdio -display none -dtb /home/air/workspace/dtb/system.dtb -s -kernel ./executable/AIRAPP.exe -S &
+PID=$!
+arm-rtems5-gdb -x $UTILS/gdb_arm_rvs_coverage-ci ./executable/AIRAPP.exe --cd=.
+
+kill -9 $PID
