50ad9c40fc0428f2aec24baa2fab091b8f9090b8
André Silva <andre.silva@gmv.com>
2023-10-10 14:02:25 +0100

    [pmk/bsp/arm/ultrascale] Updating Timer initialization, enabling/disabling

--- air/pmk/bsp/arm/ultrascale96v2_a53/global_timer.c
+++ air/pmk/bsp/arm/ultrascale96v2_a53/global_timer.c
@@ -23,69 +23,102 @@
 
 #define IOU_SCNTRS ((iou_scntrs_module *)XPAR_PSU_IOU_SCNTRS_S_AXI_BASEADDR)
 
-air_u32_t arm_determine_trigger_bit(air_u32_t us_per_tick){
-    // The counter generates a tick every N clock pulses
-    // (From Ultrascle+ TRM, Timers and Counters -> Physical Counter section)
-    air_u32_t N = (XPAR_CPU_CORTEXA53_0_CPU_CLK_FREQ_HZ / 2) / 100000000;
-
-    // This means that it generates a tick every N/CPU_CLK_FREQ seconds;
-    // So the desired period is (in ticks)
-    air_u32_t period = (us_per_tick * 1E6) / (N/XPAR_CPU_CORTEXA53_0_CPU_CLK_FREQ_HZ);
-    
-    // Find the changing bit that represents the period
-    
-    //TODO: For now assume that it is the rightmost bit of the binary representation of the period, it is only true for powers of 2
-    if (period == 0) {
-        return 0;  // No set bits
-    }
-
-    air_u32_t position = -1;
-
-    // Right shift the period until the rightmost bit is 1.
-    while ((period & 1) == 0) {
-        period >>= 1;
-        position++;
-    }
-
-    return position;
-}
-
-void arm_acknowledge_gt(void) {
-    //TODO ???
+void arm_acknowledge_gt(void) {   
+    arm_disable_global_timer();
     return;
 }
 
+void arm_set_tval(air_u32_t val){
+    //Set the TVAL in CNTP_TVAL register
+    __asm__ volatile ( "mcr p15, 0, %0, c14, c2, 0\n"::"r" (val) );
+
+    #ifdef PMK_DEBUG_TIMER
+    val = 0;
+    __asm__ volatile ( "mrc p15, 0, %0, c14, c2, 0\n":"=r" (val) );
+    printk("\t CNTP_TVAL register = 0x%x\n", val);
+    #endif
+    return;
+}
 
 void arm_init_global_timer(void) {
 
+#ifdef PMK_DEBUG_TIMER
+    printk("\n :: Generic Timer initialization\n"
+            "    IOU_SCNTRS module at 0x%x\n"
+            "    \t count_control register at 0x%x = 0x%x\n"
+            "    \t counter_status register at 0x%x\n"
+            "    \t cout_lower register at 0x%x\n"
+            "    \t count_upper register at 0x%x\n"
+            "    \t base_freq_id register at 0x%x = 0x%x\n",
+            IOU_SCNTRS, &IOU_SCNTRS->cnt_ctrl, IOU_SCNTRS->cnt_ctrl, &IOU_SCNTRS->cnt_status, &IOU_SCNTRS->current_cnt_lower, &IOU_SCNTRS->current_cnt_upper, &IOU_SCNTRS->base_freq_id, IOU_SCNTRS->base_freq_id);
+#endif
     /* in range 0-255 */
     air_u32_t us_per_tick = pmk_get_usr_us_per_tick();
 
-    //First program the counter frequency
-    // According to the registers reference we must "program this register to match the clock frequency of the timestamp generator"
-    IOU_SCNTRS->base_freq_id = 0x5F5E100; //XPAR_PSU_CORTEXA53_0_TIMESTAMP_CLK_FREQ (100MHz) in hex
-    //Should it be XPAR_PSU_CORTEXA53_0_CPU_CLK_FREQ_HZ instead?
+    air_u32_t val = IOU_SCNTRS->base_freq_id;
 
-    // Use the CNTKCTL, Counter-timer Kernel Control register to setup the timer
-    // to generate an event periodically.
-    // Set the EVNTI, bits [7:4] to select wich bit of the counter register triggers the event (select the period ?)
+    // First we need to write the freq of the system counter to the CNTFRQ register
+    __asm__ volatile ( "mcr p15, 0, %0, c14, c0, 0\n"::"r" (val) );
 
-    //TODO: Can i just write the value to the specific bits of the register or do i need to read it first and then write "what i want"|"what was alreadt there"?
-    __asm__ volatile ( "mcr p15, 0, %0, c14, c1, 0\n"::"r" (1 << arm_determine_trigger_bit(us_per_tick) ) );
+    #ifdef PMK_DEBUG_TIMER
+    val = 0;
+    __asm__ volatile ( "mrc p15, 0, %0, c14, c0, 0\n":"=r" (val) );
+    printk("\n\t CNTFRQ register = 0x%x\n", val);
+    #endif
 
-#ifdef PMK_DEBUG
-    printk("\n :: Generic Timer initialization\n"
-            "    IOU_SCNTRS module at 0x%x\n"
-            "    trigger bit = %d\n\n",
-            IOU_SCNTRS, arm_determine_trigger_bit(us_per_tick) );
-#endif
+    // Calculate and set TVAL
+    val = 0;
+
+    //The system counter generates a count after N = (XPAR_CPU_CORTEXA53_0_CPU_CLK_FREQ_HZ / 2)/100 MHz 
+    //Meaning that it generates a count every N/CPU_FREQ s = 1/200M s = (1/200M) * 1E6 us = 1/200 us
+    // 1 count --> 1/200 us
+    // x count --> us_per_tick us
+    //X = us_per_tick/(1/200) = us_per_tick*200
+    val = us_per_tick * 200;
+
+    arm_set_tval(val);
+
+    return;
+}
+
+void arm_disable_global_timer(void) {
+    //Disable the timer with CNTP_CTL
+
+    //Read what was there
+    air_u32_t val = 0;
+    __asm__ volatile ( "mrc p15, 0, %0, c14, c2, 1\n":"=r" (val) );
+
+    val &= 0b000;
+    __asm__ volatile ( "mcr p15, 0, %0, c14, c2, 1\n"::"r" (val) );
+
+    #ifdef PMK_DEBUG_TIMER
+    val = 0;
+    __asm__ volatile ( "mrc p15, 0, %0, c14, c2, 1\n":"=r" (val) );
+    printk("\t CNTP_CTL register = 0x%x\n\n", val);
+    #endif
+
+    arm_instruction_synchronization_barrier();
+    return;
 }
 
 void arm_start_global_timer(void) {
-    // Set the EVNTEN, bit [2] of the CNTP_CTL register to enable the event stream?
-    __asm__ volatile ( "mcr p15, 0, %0, c14, c2, 1\n"::"r" (1 << 2U ) );
+    //Enable the timer with CNTP_CTL
+
+    //Read what was there
+    air_u32_t val = 0;
+    __asm__ volatile ( "mrc p15, 0, %0, c14, c2, 1\n":"=r" (val) );
+
+    val |= 0b001;
+    __asm__ volatile ( "mcr p15, 0, %0, c14, c2, 1\n"::"r" (val) );
+
+    #ifdef PMK_DEBUG_TIMER
+    val = 0;
+    __asm__ volatile ( "mrc p15, 0, %0, c14, c2, 1\n":"=r" (val) );
+    printk("\t CNTP_CTL register = 0x%x\n\n", val);
+    #endif
 
     arm_instruction_synchronization_barrier();
+    return;
 }
 
 air_u64_t arm_read_global_timer(void) {
@@ -104,9 +137,47 @@ air_u64_t arm_read_global_timer(void) {
     return result;
 }
 
-// ??
-// void arm_acknowledge_gt(void) {
-//     GT->irq_status = 0x1;
+// air_u32_t arm_determine_trigger_bit(air_u32_t us_per_tick){
+//     // The counter generates a tick every N clock pulses
+//     // (From Ultrascle+ TRM, Timers and Counters -> Physical Counter section)
+//     air_u32_t N = (XPAR_CPU_CORTEXA53_0_CPU_CLK_FREQ_HZ / 2) / 100E6;
+
+// #ifdef PMK_DEBUG_TIMER
+//     printk("        :: global timer trigger bit calculation\n"
+//            "           us_per_tick = %d\n"
+//            "           CPU clock freq = %d\n"
+//            "           N = %d\n", us_per_tick, XPAR_CPU_CORTEXA53_0_CPU_CLK_FREQ_HZ, N);
+// #endif
+
+//     // This means that every (N/CPU_CLK_FREQ) * 1E6 microseconds a bit changes in the count
+//     // We want to know how many counter ticks are equivalent to us_per_tick microseconds 
+
+//     // 1 counter tick --> (N/CPU_CLK_FREQ) * 1E6 microseconds
+//     // X counter ticks --> us_per_tick microseconds
+
+//     // So the desired period is (counter ticks)
+//     air_u32_t period = (us_per_tick) / ( ( (float) N / (float) XPAR_CPU_CORTEXA53_0_CPU_CLK_FREQ_HZ) * 1E6);
+    
+// #ifdef PMK_DEBUG_TIMER
+//     printk("           calculated period = %d\n\n", period);
+// #endif
+
+//     // Find the changing bit that represents the period
+    
+//     //TODO: For now assume that it is the rightmost bit of the binary representation of the period, it is only true for powers of 2
+//     if (period == 0) {
+//         return 0;  // No set bits
+//     }
+
+//     air_u32_t position = 0;
+
+//     // Right shift the period until the rightmost bit is 1.
+//     while ((period & 0b01) == 0) {
+//         period >>= 1;
+//         position++;
+//     }
+
+//     return position;
 // }
 
 // air_u32_t arm_determine_gt_prescaler(air_u32_t us_per_tick) {
