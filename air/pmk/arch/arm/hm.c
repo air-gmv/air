a851632b2fe5563a8ca1056da0047f7bebdd7409
Carolina Serra <cpserra@gmv.com>
2024-02-22 17:35:57 +0000

    [ARM] Added LR virtualization to HM handler

--- air/pmk/arch/arm/hm.c
+++ air/pmk/arch/arm/hm.c
@@ -170,6 +170,8 @@ static air_uptr_t *arm_partition_hm_handler(air_u32_t id, pmk_core_ctrl_t *core)
             vcpu->psr &= ~(ARM_PSR_MODE_MASK);
             vcpu->psr |= ARM_PSR_IRQ;
             core->context->sp_svc = frame->usr_sp;
+            core->context->usr_svc_lr = frame->usr_lr;
+            core->context->usr_irq_lr = frame->ret_addr;
         }
     }
 
