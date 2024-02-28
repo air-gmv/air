33ed0dabd5946a67dcb1c1b9b40bab164f408be0
André Silva <andre.silva@gmv.com>
2024-02-28 13:39:41 +0000

    [utils/hw_scripts/ultrascale] Update test_folder path in launch.py

--- utils/hw_scripts/Ultrascale/launch.py
+++ utils/hw_scripts/Ultrascale/launch.py
@@ -72,7 +72,7 @@ def main():
     group.add_argument('-a', '--all_tests', action='store_true', help='Run all tests')
     parser.add_argument('-l', '--list_tests', action='store_true', help='List available tests')
 
-    test_folder = "/home/ansi/AIR_ULTRASCALE/2peruair/air/examples/private-example/AIR_testsuite"  # Update with the actual path to your test folder
+    test_folder = f"{os.environ['AIR']}{TESTS_FOLDER}"
     
     # List available tests (subfolders)
     available_tests = list_tests(test_folder)
